21b8037ee66f1c71c595b8a84cd3b7ecd5da52c5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-24 20:09:41 -0300

    New column FullName

--- Api/models/entities/UserEntity.cs
+++ Api/models/entities/UserEntity.cs
@@ -4,6 +4,7 @@ namespace Api.models.entities;
 
 public class UserEntity: IdentityUser
 {
+    public string? FullName { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpiryTime { get; set; }
 }
