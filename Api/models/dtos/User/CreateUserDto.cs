701ee2267c1b54e47c3bc811b6bdecefdbe5f297
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-24 20:51:17 -0300

    New CreateUserDto

---
+++ Api/models/dtos/User/CreateUserDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.models.dtos.Users;
+
+public class CreateUserDto
+{
+    [Required(ErrorMessage = "Field is required")]
+    [StringLength(100, ErrorMessage = "Max size of 100")]
+    public string Name { get; set; } = string.Empty; 
+
+    [Required(ErrorMessage = "Field is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email")]
+    [StringLength(150, ErrorMessage = "Max size of 150")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Field is required")]
+    [StringLength(50, ErrorMessage = "Max size of 50", MinimumLength = 6)] 
+    public string Password { get; set; } = string.Empty;
+}
+    
