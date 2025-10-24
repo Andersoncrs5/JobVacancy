e1a5c4a2babd4a4228de7bd11182a8aef180cf39
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-24 20:06:52 -0300

    Impled configs in OnModelCreating

--- Api/Context/AppDbContext.cs
+++ Api/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Api.models.entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,13 @@ public class AppDbContext(DbContextOptions<AppDbContext> options): IdentityDbCon
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<UserEntity>().ToTable("app_users");
+        modelBuilder.Entity<IdentityRole>().ToTable("app_roles");
+        modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("app_user_claims");
+        modelBuilder.Entity<IdentityUserRole<string>>().ToTable("app_user_roles");
+        modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("app_user_logins");
+        modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("app_role_claims");
+        modelBuilder.Entity<IdentityUserToken<string>>().ToTable("app_user_tokens");
     }
 }
