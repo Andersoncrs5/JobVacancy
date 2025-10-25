3b9cb50cc874ec9d2fb09936f29b550e9a8294a2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-25 15:08:42 -0300

    Impled and configured CustomWebApplicationFactory

---
+++ Tests/Api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Api;
+using Api.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                AppDbContext context = scopedServices.GetRequiredService<AppDbContext>();
+                try
+                {
+                    context.Database.EnsureCreated();
+                    context.Database.Migrate();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error occured while seeding the database. {0} ", e);
+                    throw;
+                }
+            }
+        });
+    }
+}
