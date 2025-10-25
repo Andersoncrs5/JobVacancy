0731ddd842d3f8254b5c6a3c69070c7f601dece0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-25 14:26:38 -0300

    New reference to RoleRepository

--- Api/Utils/Uow/Interfaces/IUnitOfWork.cs
+++ Api/Utils/Uow/Interfaces/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace Api.Utils.Uow.Interfaces;
 public interface IUnitOfWork: IDisposable
 {
     IUserRepository UserRepository { get; }
+    IRoleRepository RoleRepository { get; }
 
     Task Commit();
 }
