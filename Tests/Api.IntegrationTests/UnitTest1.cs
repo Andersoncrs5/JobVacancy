07d1f900dc85ba5b84b64dc62fc1b2efb535aa73
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-25 15:09:09 -0300

    Configured UnitTest1 to integration test

---
+++ Tests/Api.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using FluentAssertions;
+
+namespace Api.IntegrationTests;
+
+public class UnitTest1: IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public UnitTest1(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+    
+    [Fact]
+    public async Task Test1()
+    {
+        HttpResponseMessage response = await _client.GetAsync("/api/v1/Auth");
+
+        string responseString = await response.Content.ReadAsStringAsync();
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    }
+}
