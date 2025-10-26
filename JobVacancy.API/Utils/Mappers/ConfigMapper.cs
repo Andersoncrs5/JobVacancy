80f4720472dbb6190d0c466b8383d7e5db7bf124
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-26 11:59:28 -0300

    Change class name

--- JobVacancy.API/Utils/Mappers/ConfigMapper.cs
+++ JobVacancy.API/Utils/Mappers/ConfigMapper.cs
@@ -2,14 +2,16 @@ using JobVacancy.API.models.dtos;
 using JobVacancy.API.models.dtos.Users;
 using JobVacancy.API.models.entities;
 using AutoMapper;
+using JobVacancy.API.Utils.Page;
 
 namespace JobVacancy.API.Utils.Mappers;
 
-public class UserMapper: Profile
+public class ConfigMapper: Profile
 {
-    public UserMapper()
+    public ConfigMapper()
     {
         CreateMap<CreateUserDto, UserEntity>();
         CreateMap<UserEntity, UserDto>();
+        CreateMap(typeof(PaginatedList<>), typeof(Page<>));
     }
 }
