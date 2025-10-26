fb2fd22a0b65eb7a6494467ffa5d0a1f4315e3fd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-26 12:00:19 -0300

    More Tests

--- Tests/Api.IntegrationTests/UserTests/UserControllerTest.cs
+++ Tests/Api.IntegrationTests/UserTests/UserControllerTest.cs
@@ -1,7 +1,10 @@
+using System.Net;
 using System.Net.Http.Json;
 using FluentAssertions;
 using JobVacancy.API.IntegrationTests.Utils;
 using JobVacancy.API.models.dtos.Users;
+using JobVacancy.API.Utils.Page;
+using JobVacancy.API.Utils.Res;
 
 namespace JobVacancy.API.IntegrationTests.UserTests;
 
@@ -17,11 +20,59 @@ public class UserControllerTest: IClassFixture<CustomWebApplicationFactory>
     }
     
     [Fact]
+    public async Task GetSingleUserById()
+    {
+        UserResultTest result = await _helper.CreateAndGetUser();
+        result.Should().NotBeNull();
+        result.User.Should().NotBeNull();
+        result.Tokens.Should().NotBeNull();
+        result.Tokens.Token.Should().NotBeNull();
+        
+        string token = result.Tokens.Token;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        
+        HttpResponseMessage getById = await _client.GetAsync($"/api/v1/User?id={result.User.Id}");
+        getById.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        Page<UserDto>? page = await getById.Content.ReadFromJsonAsync<Page<UserDto>>();
+        page.Should().NotBeNull();
+        page.Data.Should().NotBeNull();
+        page.Data.First().Should().BeEquivalentTo(result.User);
+    }
+    
+    [Fact]
+    public async Task GetAllUsers()
+    {
+        UserResultTest result = await _helper.CreateAndGetUser();
+        result.Should().NotBeNull();
+        result.User.Should().NotBeNull();
+        result.Tokens.Should().NotBeNull();
+        result.Tokens.Token.Should().NotBeNull();
+        
+        string token = result.Tokens.Token;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        
+        HttpResponseMessage getByEmail = await _client.GetAsync($"/api/v1/User?email={result.User.Email}");
+        getByEmail.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        HttpResponseMessage getByUsername = await _client.GetAsync($"/api/v1/User?username={result.User.Username}");
+        getByUsername.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        HttpResponseMessage getByFullname = await _client.GetAsync($"/api/v1/User?fullName={result.User.FullName}");
+        getByFullname.StatusCode.Should().Be(HttpStatusCode.OK);
+
+    }
+    
+    [Fact]
     public async Task UpdateUser()
     {
+        var num = Random.Shared.Next(1, 100000000);
+
         UpdateUserDto dto = new UpdateUserDto
         {
-            Name = "userupdated12365",
+            Name = $"pochita{num}",
             FullName = "pochita chainsaw demon",
             Password = "4873586275&&$vfsHi"
         };
