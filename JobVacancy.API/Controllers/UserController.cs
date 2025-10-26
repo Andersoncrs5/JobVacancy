a9cca98861c9b1ca7f512030268b7e57ac43f493
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-26 11:38:36 -0300

    New endpoint GetAllUser

--- JobVacancy.API/Controllers/UserController.cs
+++ JobVacancy.API/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using JobVacancy.API.models.dtos.Users;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.User;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -79,6 +81,47 @@ public class UserController(
                 TraceId = HttpContext.TraceIdentifier,
                 Version = 1
             });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(PaginatedList<UserDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [EnableRateLimiting("GetItemPolicy")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> GetAllUser([FromQuery] UserFilterParams filter)
+    {
+        try
+        {
+            string? id = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (id == null) return Unauthorized();
+
+            var queryable = userService.GetIQueryable();
+            IQueryable<UserEntity> userEntities = UserFilterQuery.ApplyFilter(queryable, filter);
+            PaginatedList<UserEntity> list = await PaginatedList<UserEntity>.CreateAsync(
+                source: userEntities, 
+                pageIndex: filter.PageNumber,
+                pageSize: filter.PageSize    
+            );
+            
+            PaginatedList<UserDto> paginatedDtos = PaginatedList<UserEntity>.MapTo<UserEntity, UserDto>(list, mapper);
+
+            return Ok(paginatedDtos);
         }
         catch (Exception e)
         {
