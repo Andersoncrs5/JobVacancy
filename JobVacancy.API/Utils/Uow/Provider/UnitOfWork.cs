f262616e30c54d2fcf7ec689cc1a7cd5fd72f003
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 14:19:59 -0300

    New New method BeginTransactionAsync and ExecuteTransactionAsync. And new refenrence CategoryRepository

--- JobVacancy.API/Utils/Uow/Provider/UnitOfWork.cs
+++ JobVacancy.API/Utils/Uow/Provider/UnitOfWork.cs
@@ -4,6 +4,7 @@ using JobVacancy.API.Repositories.Interfaces;
 using JobVacancy.API.Repositories.Provider;
 using JobVacancy.API.Utils.Uow.Interfaces;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace JobVacancy.API.Utils.Uow.Provider;
 
@@ -15,15 +16,57 @@ public class UnitOfWork(
 {
     private UserRepository? _userRepository;
     private RoleRepository? _roleRepository;
+    private CategoryRepository? _categoryRepository;
 
     public IUserRepository UserRepository
         => _userRepository ??= new UserRepository(context, userManager);
 
     public IRoleRepository RoleRepository
         => _roleRepository ??= new RoleRepository(roleManager);
+    
+    public ICategoryRepository CategoryRepository
+        => _categoryRepository ??= new CategoryRepository(context);
 
     public async Task Commit() => await context.SaveChangesAsync();
+    
+    public Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        return context.Database.BeginTransactionAsync();
+    }
 
-    public void Dispose() => context.Dispose();
+    public void Dispose()
+    {
+        context.Dispose();
+        GC.SuppressFinalize(this);
+    }
+    
+    public async Task ExecuteTransactionAsync(Func<Task> operation)
+    {
+        IDbContextTransaction? transaction = null;
+
+        try
+        {
+            transaction = await BeginTransactionAsync();
+
+            await operation.Invoke();
+
+            await transaction.CommitAsync();
+        }
+        catch (Exception)
+        {
+            if (transaction != null)
+            {
+                await transaction.RollbackAsync();
+            }
+            throw; 
+        }
+        finally
+        {
+            if (transaction != null)
+            {
+                await transaction.DisposeAsync();
+            }
+        }
+    }
         
 }
