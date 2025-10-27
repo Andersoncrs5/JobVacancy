cac8f9ab5d4c0eb84d7d199007af80546b12706d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 14:16:51 -0300

    new CategoryFilterParams

---
+++ JobVacancy.API/Utils/Filters/Category/CategoryFilterParams.cs
@@ -0,0 +1,8 @@
+namespace JobVacancy.API.Utils.Filters.Category;
+
+public class CategoryFilterParams: FilterBaseParams
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool? IsActive { get; set; }
+}
