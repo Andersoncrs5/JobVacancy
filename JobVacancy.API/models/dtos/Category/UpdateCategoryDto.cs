548ad10dcf9c3f8090027b8b1a9e292bc2a11647
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 20:29:19 -0300

    Improvements in types

--- JobVacancy.API/models/dtos/Category/UpdateCategoryDto.cs
+++ JobVacancy.API/models/dtos/Category/UpdateCategoryDto.cs
@@ -2,7 +2,7 @@ namespace JobVacancy.API.models.dtos.Category;
 
 public class UpdateCategoryDto
 {
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public bool? IsActive { get; set; } = true;
 }
--- JobVacancy.API/models/dtos/User/UpdateUserDto.cs
+++ JobVacancy.API/models/dtos/User/UpdateUserDto.cs
@@ -5,11 +5,11 @@ namespace JobVacancy.API.models.dtos.Users;
 public class UpdateUserDto
 {
     [StringLength(100, ErrorMessage = "Max size of 100")]
-    public string Name { get; set; } = string.Empty; 
+    public string? Name { get; set; } 
 
     [StringLength(200, ErrorMessage = "Max size of 100")]
-    public string? FullName { get; set; } = string.Empty; 
+    public string? FullName { get; set; } 
     
     [StringLength(50, ErrorMessage = "Max size of 50", MinimumLength = 6)] 
-    public string Password { get; set; } = string.Empty;
+    public string? Password { get; set; }
 }
