6aef82dbab988456ef01ad96bfe048a2b47a243f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 15:40:49 -0300

    new method helper LoginMaster, LoginUser and _Login

--- Tests/Api.IntegrationTests/Helper.cs
+++ Tests/Api.IntegrationTests/Helper.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using JobVacancy.API.IntegrationTests.Utils;
 using JobVacancy.API.models.dtos.Users;
 using JobVacancy.API.Utils.Res;
+using Microsoft.Extensions.Configuration;
 
 namespace JobVacancy.API.IntegrationTests;
 
@@ -11,6 +12,44 @@ public class Helper(
     HttpClient client
     )
 {
+    public async Task<ResponseTokens> LoginMaster(IConfiguration configuration)
+    {
+        var datasSystemSection = configuration.GetSection("DataSystem");
+        string systemUserEmail = datasSystemSection["systemUserEmail"] ?? throw new InvalidOperationException("System user email configuration is missing.");
+        string systemUserPassword = datasSystemSection["SystemUserPassword"] ?? throw new InvalidOperationException("System user password configuration is missing.");
+        
+        var http = await _Login(systemUserEmail, systemUserPassword);
+        return http.Data!;
+    }
+    public async Task<ResponseTokens> LoginUser(string email, string password)
+    {
+        var http = await _Login(email, password);
+        http.Data.Should().NotBeNull();
+        return http.Data;
+    }
+
+    private async Task<ResponseHttp<ResponseTokens>> _Login(string email, string password)
+    {
+        LoginDto dto = new LoginDto
+        {
+            Email = email,
+            Password = password
+        };
+        
+        HttpResponseMessage response = await client.PostAsJsonAsync($"/api/v1/Auth/Login", dto);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        response.Should().NotBeNull();
+        
+        ResponseHttp<ResponseTokens>? content = await response.Content.ReadFromJsonAsync<ResponseHttp<ResponseTokens>>();
+        
+        content.Should().NotBeNull();
+        content.Data.Should().NotBeNull();
+        content.Data.Token.Should().NotBeNull();
+        content.Data.RefreshToken.Should().NotBeNull();
+        
+        return content;
+    }
+    
     public async Task<UserResultTest> CreateAndGetUser()
     {
         int num = Random.Shared.Next(1, 100000000);
