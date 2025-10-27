cf94ad276a81cf12a5a879e159e9a14646b83438
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 14:16:37 -0300

    added  = string.Empty; to column Name

--- JobVacancy.API/models/entities/CategoryEntity.cs
+++ JobVacancy.API/models/entities/CategoryEntity.cs
@@ -2,7 +2,7 @@ namespace JobVacancy.API.models.entities;
 
 public class CategoryEntity: BaseEntity
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
     public bool IsActive { get; set; }
 }
