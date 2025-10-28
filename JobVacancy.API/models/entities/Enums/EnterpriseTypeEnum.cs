1c8f31180e79658e06498c132e66501d4c5045e7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-28 14:04:58 -0300

    New enum EnterpriseTypeEnum was added in table EnterpriseEntity

---
+++ JobVacancy.API/models/entities/Enums/EnterpriseTypeEnum.cs
@@ -0,0 +1,16 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum EnterpriseTypeEnum
+{
+    Undefined = 0, 
+    SoleProprietorship = 1,     
+    Partnership = 2,            
+    LimitedLiability = 3,       
+    Corporation = 4,            
+    NonProfit = 5,              
+    Startup = 6,                
+    SmallBusiness = 7,          
+    MediumBusiness = 8,         
+    LargeEnterprise = 9,        
+    MicroEnterprise = 10,
+}
