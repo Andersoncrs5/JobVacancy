98a72ff26abfba650af1b1e8f5fa57d5370b5293
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-27 21:23:35 -0300

    New IndustryRepository

--- JobVacancy.API/Utils/Uow/Interfaces/IUnitOfWork.cs
+++ JobVacancy.API/Utils/Uow/Interfaces/IUnitOfWork.cs
@@ -8,6 +8,8 @@ public interface IUnitOfWork: IDisposable
     IUserRepository UserRepository { get; }
     IRoleRepository RoleRepository { get; }
     ICategoryRepository CategoryRepository { get; }
+    IIndustryRepository IndustryRepository { get; }
+    
 
     Task Commit();
     Task<IDbContextTransaction> BeginTransactionAsync();
