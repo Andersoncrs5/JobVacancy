90e6d59212d1e5657c0a9a09f4fa31310ff4995d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-28 20:15:30 -0300

    New column IsPrimary

--- JobVacancy.API/models/entities/EnterpriseIndustryEntity.cs
+++ JobVacancy.API/models/entities/EnterpriseIndustryEntity.cs
@@ -2,6 +2,7 @@ namespace JobVacancy.API.models.entities;
 
 public class EnterpriseIndustryEntity: BaseEntity
 {
+    public bool IsPrimary  { get; set; }
     public string EnterpriseId { get; set; }
     public EnterpriseEntity Enterprise { get; set; }
     
