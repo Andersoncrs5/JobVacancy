4b05cc1c7b6ee884a19cdd2727b7090df45cad90
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-28 20:27:59 -0300

    New table EnterpriseIndustryEntity

--- JobVacancy.API/models/entities/EnterpriseIndustryEntity.cs
+++ JobVacancy.API/models/entities/EnterpriseIndustryEntity.cs
@@ -4,8 +4,10 @@ public class EnterpriseIndustryEntity: BaseEntity
 {
     public bool IsPrimary  { get; set; }
     public string EnterpriseId { get; set; }
-    public EnterpriseEntity Enterprise { get; set; }
+    
+    public virtual EnterpriseEntity Enterprise { get; set; } 
     
     public string IndustryId { get; set; }
-    public IndustryEntity Industry { get; set; }
+    
+    public virtual IndustryEntity Industry { get; set; }
 }
