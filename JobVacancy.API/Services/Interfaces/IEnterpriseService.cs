f84fdaa359ce1692d807e36df4c39a7b761d671c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-28 20:07:20 -0300

    Impled a lots of method

--- JobVacancy.API/Services/Interfaces/IEnterpriseService.cs
+++ JobVacancy.API/Services/Interfaces/IEnterpriseService.cs
@@ -1,6 +1,17 @@
+using JobVacancy.API.models.dtos.Enterprise;
+using JobVacancy.API.models.entities;
+
 namespace JobVacancy.API.Services.Interfaces;
 
 public interface IEnterpriseService
 {
-    
+    Task<EnterpriseEntity?> GetById(string id);
+    Task<EnterpriseEntity?> GetByUserId(string userId);
+    void Delete(EnterpriseEntity entity);
+    Task<bool> ExistsById(string id);
+    Task<bool> ExistsByUserId(string userId);
+    Task<bool> ExistsByName(string name);
+    IQueryable<EnterpriseEntity> Query();
+    Task<EnterpriseEntity> CreateAsync(CreateEnterpriseDto dto, string userId);
+    Task<EnterpriseEntity> UpdateAsync(EnterpriseEntity entity, UpdateEnterpriseDto dto);
 }
--- JobVacancy.API/Services/Providers/EnterpriseService.cs
+++ JobVacancy.API/Services/Providers/EnterpriseService.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using JobVacancy.API.models.dtos.Enterprise;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
 using JobVacancy.API.Utils.Uow.Interfaces;
@@ -10,6 +11,62 @@ public class EnterpriseService(IUnitOfWork uow, IMapper mapper): IEnterpriseServ
     public async Task<EnterpriseEntity?> GetById(string id)
     {
         return await uow.EnterpriseRepository.GetByIdAsync(id);
+    }
+
+    public async Task<EnterpriseEntity?> GetByUserId(string userId)
+    {
+        return await uow.EnterpriseRepository.GetByUserId(userId);
+    }
+
+    public void Delete(EnterpriseEntity entity)
+    {
+        uow.EnterpriseRepository.Delete(entity);
+    }
+
+    public async Task<bool> ExistsById(string id)
+    {
+        return await uow.EnterpriseRepository.ExistsById(id);
+    }
+    
+    public async Task<bool> ExistsByUserId(string userId)
+    {
+        return await uow.EnterpriseRepository.ExistsByUserId(userId);
+    }
+    
+    public async Task<bool> ExistsByName(string name)
+    {
+        return await uow.EnterpriseRepository.ExistsByName(name);
+    }
+
+    public IQueryable<EnterpriseEntity> Query()
+    {
+        return uow.EnterpriseRepository.ReturnIQueryable();
+    }
+
+    public async Task<EnterpriseEntity> CreateAsync(CreateEnterpriseDto dto, string userId)
+    {
+        var entity = mapper.Map<EnterpriseEntity>(dto);
+        entity.UserId = userId;
+        
+        EnterpriseEntity enterprise = await uow.EnterpriseRepository.AddAsync(entity);
+        await uow.Commit();
+        return enterprise;
+    }
+
+    public async Task<EnterpriseEntity> UpdateAsync(EnterpriseEntity entity, UpdateEnterpriseDto dto)
+    {
+        var originalType = entity.Type; 
+
+        mapper.Map(dto, entity);
+    
+        if (!dto.Type.HasValue) 
+        {
+            entity.Type = originalType;
+        }
+        
+        EnterpriseEntity enterprise = await uow.EnterpriseRepository.Update(entity);
+        await uow.Commit();
+        return entity;
     }
     
     
