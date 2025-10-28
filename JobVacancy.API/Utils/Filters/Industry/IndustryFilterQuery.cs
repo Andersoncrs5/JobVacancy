4759b5d97e7cd220a057a7837706da11e7e15cb9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-28 11:29:45 -0300

    Added IndustryFilterQuery

---
+++ JobVacancy.API/Utils/Filters/Industry/IndustryFilterQuery.cs
@@ -0,0 +1,29 @@
+using JobVacancy.API.models.entities;
+using JobVacancy.API.Utils.Filters.Category;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobVacancy.API.Utils.Filters.Industry;
+
+public class IndustryFilterQuery
+{
+    public static IQueryable<IndustryEntity> ApplyFilter(IQueryable<IndustryEntity> query, IndustryFilterParams filter)
+    {
+        if (!string.IsNullOrWhiteSpace(filter.Name))
+        {
+            query = query.Where(c => EF.Functions.Like(c.Name, $"%{filter.Name}%"));
+            
+        }
+        
+        if (!string.IsNullOrWhiteSpace(filter.Description))
+        {
+            query = query.Where(c => EF.Functions.Like(c.Description, $"%{filter.Description}%"));
+        }
+        
+        if (filter.IsActive.HasValue)
+        {
+            query = query.Where(c => c.IsActive == filter.IsActive);
+        }
+        
+        return FilterBaseQuery.ApplyBaseFilters(query, filter);
+    }
+}
