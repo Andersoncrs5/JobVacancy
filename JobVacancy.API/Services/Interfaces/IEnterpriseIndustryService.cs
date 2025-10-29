a609ca30f3dc74508fca3487c346e79e3e7aed23
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-29 15:02:08 -0300

    New EnterpriseIndustryService interface and provider

---
+++ JobVacancy.API/Services/Interfaces/IEnterpriseIndustryService.cs
@@ -0,0 +1,15 @@
+using JobVacancy.API.models.dtos.EnterpriseIndustry;
+using JobVacancy.API.models.entities;
+
+namespace JobVacancy.API.Services.Interfaces;
+
+public interface IEnterpriseIndustryService
+{
+    Task<EnterpriseIndustryEntity?> GetByIdAsync(string id);
+    Task<EnterpriseIndustryEntity?> GetByIndustryIdAndEnterpriseId(string industryId, string enterpriseId);
+    Task<bool> ExistsByIndustryIdAndEnterpriseId(string industryId, string enterpriseId);
+    Task<EnterpriseIndustryEntity> CreateAsync(CreateEnterpriseIndustryDto dto);
+    void Delete(EnterpriseIndustryEntity entity);
+    IQueryable<EnterpriseIndustryEntity> GetQuery();
+    Task<int> CheckAmount(string enterpriseId);
+}
---
+++ JobVacancy.API/Services/Providers/EnterpriseIndustryService.cs
@@ -0,0 +1,61 @@
+using AutoMapper;
+using JobVacancy.API.models.dtos.EnterpriseIndustry;
+using JobVacancy.API.models.entities;
+using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Uow.Interfaces;
+
+namespace JobVacancy.API.Services.Providers;
+
+public class EnterpriseIndustryService(IUnitOfWork uow, IMapper mapper): IEnterpriseIndustryService
+{
+    public async Task<EnterpriseIndustryEntity?> GetByIdAsync(string id)
+    {
+        return await uow.EnterpriseIndustryRepository.GetByIdAsync(id);
+    }
+
+    public async Task<EnterpriseIndustryEntity?> GetByIndustryIdAndEnterpriseId(string industryId, string enterpriseId)
+    {
+        return await uow.EnterpriseIndustryRepository.GetByIndustryIdAndEnterpriseId(industryId, enterpriseId);
+    }
+    
+    public async Task<bool> ExistsByIndustryIdAndEnterpriseId(string industryId, string enterpriseId)
+    {
+        return await uow.EnterpriseIndustryRepository.ExistsByIndustryIdAndEnterpriseId(industryId, enterpriseId);
+    }
+
+    public async Task<EnterpriseIndustryEntity> CreateAsync(CreateEnterpriseIndustryDto dto)
+    {
+        int amount = await CheckAmount(dto.EnterpriseId);
+        if (amount > 15)
+        {
+            throw new Exception("Enterprise industry amount exceed");
+        }
+
+        EnterpriseIndustryEntity entity = new EnterpriseIndustryEntity
+        {
+            EnterpriseId = dto.EnterpriseId,
+            IndustryId = dto.IndustryId,
+            IsPrimary = dto.IsPrimary,
+        };
+
+        EnterpriseIndustryEntity async = await uow.EnterpriseIndustryRepository.AddAsync(entity);
+        await uow.Commit();
+        return async;
+    }
+
+    public async Task<int> CheckAmount(string enterpriseId)
+    {
+        return await uow.EnterpriseIndustryRepository.GetAmountIndustryByEnterpriseId(enterpriseId);
+    }
+
+    public void Delete(EnterpriseIndustryEntity entity)
+    {
+        uow.EnterpriseIndustryRepository.Delete(entity);
+    }
+
+    public IQueryable<EnterpriseIndustryEntity> GetQuery()
+    {
+        return uow.EnterpriseIndustryRepository.ReturnIQueryable();
+    }
+    
+}
