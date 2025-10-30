d00914ff574219c41a74d30c058a8625f19451d4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 19:53:19 -0300

    New base: BasePost

---
+++ JobVacancy.API/models/dtos/BasePost.cs
@@ -0,0 +1,12 @@
+namespace JobVacancy.API.models.dtos;
+
+public class BasePost: BaseDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsFeatured { get; set; } = false;
+    public int? ReadingTimeMinutes { get; set; }
+    public string CategoryId { get; set; } = string.Empty;
+}
