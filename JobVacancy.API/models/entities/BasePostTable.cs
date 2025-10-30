bbac11edd56e261fe887cbfc66a4598da3cb81d6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 18:15:50 -0300

    New base BasePostTable to Table post

---
+++ JobVacancy.API/models/entities/BasePostTable.cs
@@ -0,0 +1,11 @@
+namespace JobVacancy.API.models.entities;
+
+public abstract class BasePostTable: BaseEntity
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public bool IsActive { get; set; } = true;
+    public bool IsFeatured { get; set; } = false;
+    public int? ReadingTimeMinutes { get; set; }
+}
