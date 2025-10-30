504bbbbc33b6ce9d39f3e340bcd0a958deacf281
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 19:54:13 -0300

    New dto CreatePostUserDto

---
+++ JobVacancy.API/models/dtos/PostUser/CreatePostUserDto.cs
@@ -0,0 +1,13 @@
+namespace JobVacancy.API.models.dtos.PostUser;
+
+public class CreatePostUserDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsFeatured { get; set; }
+    public int? ReadingTimeMinutes { get; set; }
+    public string CategoryId { get; set; } = string.Empty;
+    
+}
