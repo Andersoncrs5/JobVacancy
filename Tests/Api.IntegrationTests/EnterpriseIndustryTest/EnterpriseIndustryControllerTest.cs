879bc4d90a2506ef2a25034c9680337ad35fe298
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 17:35:14 -0300

    More tests

--- Tests/Api.IntegrationTests/EnterpriseIndustryTest/EnterpriseIndustryControllerTest.cs
+++ Tests/Api.IntegrationTests/EnterpriseIndustryTest/EnterpriseIndustryControllerTest.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
 using FluentAssertions;
 using JobVacancy.API.IntegrationTests.Utils;
 using JobVacancy.API.models.dtos.Enterprise;
@@ -8,6 +9,7 @@ using JobVacancy.API.models.dtos.Industry;
 using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
 
 namespace JobVacancy.API.IntegrationTests.EnterpriseIndustryTest;
 
@@ -17,12 +19,14 @@ public class EnterpriseIndustryControllerTest: IClassFixture<CustomWebApplicatio
     private readonly Helper _helper;
     private readonly IConfiguration _configuration;
     private readonly string _URL = "/api/v1/EnterpriseIndustry";
+    private readonly ITestOutputHelper _output;
 
-    public EnterpriseIndustryControllerTest(CustomWebApplicationFactory factory) 
+    public EnterpriseIndustryControllerTest(CustomWebApplicationFactory factory, ITestOutputHelper output) 
     {
         _client = factory.CreateClient(); 
         _helper = new Helper(_client); 
         _configuration = factory.Configuration;
+        _output = output;
     }
 
     [Fact]
@@ -235,8 +239,170 @@ public class EnterpriseIndustryControllerTest: IClassFixture<CustomWebApplicatio
         
         HttpResponseMessage message = await _client.DeleteAsync("/api/v1/EnterpriseIndustry/" +  industryDto.Id);
         message.StatusCode.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Fact]
+    public async Task TogglePrimaryFalse()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
         
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        string saved = await _helper.CreateEnterpriseIndustry(enterprise.Id, industryDto.Id, true);
+
+        HttpResponseMessage message = await _client.PatchAsync($"{_URL}/{saved}/toggle/status/is-primary", new StringContent("{}", Encoding.UTF8, "application/json"));
+        
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+        ResponseHttp<bool>? content = await message.Content.ReadFromJsonAsync<ResponseHttp<bool>>();
+        content.Should().NotBeNull();
+        content.Status.Should().BeTrue();
+        
+        content.Data.Should().BeFalse();
     }
     
+    [Fact]
+    public async Task TogglePrimaryTrue()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        string saved = await _helper.CreateEnterpriseIndustry(enterprise.Id, industryDto.Id, false);
+
+        HttpResponseMessage message = await _client.PatchAsync($"{_URL}/{saved}/toggle/status/is-primary", new StringContent("{}", Encoding.UTF8, "application/json"));
+        
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+        ResponseHttp<bool>? content = await message.Content.ReadFromJsonAsync<ResponseHttp<bool>>();
+        content.Should().NotBeNull();
+        content.Status.Should().BeTrue();
+        
+        content.Data.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task TogglePrimaryReturnForbidden()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        string saved = await _helper.CreateEnterpriseIndustry(enterprise.Id, industryDto.Id, false);
+        
+        token = user.Tokens!.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        HttpResponseMessage message = await _client.PatchAsync($"{_URL}/{saved}/toggle/status/is-primary", new StringContent("{}", Encoding.UTF8, "application/json"));
+        message.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+    
+    [Fact]
+    public async Task TogglePrimaryReturnNotFound()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        string saved = await _helper.CreateEnterpriseIndustry(enterprise.Id, industryDto.Id, false);
+
+        HttpResponseMessage message = await _client.PatchAsync($"{_URL}/{Guid.NewGuid()}/toggle/status/is-primary", new StringContent("{}", Encoding.UTF8, "application/json"));
+        
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        ResponseHttp<object>? content = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        
+        content.Should().NotBeNull();
+        content.Status.Should().BeFalse();
+        content.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task ToggleSuccessCreatesLinkReturns201()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master); // Industry existe
+    
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+    
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        HttpResponseMessage message = await _client.PostAsync($"{_URL}/{industryDto.Id}/toggle", null);
+
+        message.StatusCode.Should().Be(HttpStatusCode.Created);
+        ResponseHttp<string>? response = await message.Content.ReadFromJsonAsync<ResponseHttp<string>>();
+        response.Should().NotBeNull();
+        response.Status.Should().BeTrue();
+        response.Data.Should().NotBeNullOrEmpty(); // Espera o ID da nova relação
+        response.Message.Should().Be("Industry added with successfully");
+    }
+    
+    [Fact]
+    public async Task ToggleSuccessDeletesLinkReturns200()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+    
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        var loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        string token = loginUserWithNewRole.Token!;
+        _client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+        await _helper.CreateEnterpriseIndustry(enterprise.Id, industryDto.Id, false); 
+    
+        HttpResponseMessage message = await _client.PostAsync($"{_URL}/{industryDto.Id}/toggle", null);
+
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+        ResponseHttp<object>? response = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        response.Should().NotBeNull();
+        response.Status.Should().BeTrue();
+        response.Data.Should().BeNull();
+        response.Message.Should().Be("Industry removed with successfully");
+    }
+    
+    [Fact]
+    public async Task ToggleFailNoTokenReturns401()
+    {
+        _client.DefaultRequestHeaders.Authorization = null;
+        HttpResponseMessage message = await _client.PostAsync($"{_URL}/{Guid.NewGuid()}/toggle", null);
+        message.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+    }
     
 }
