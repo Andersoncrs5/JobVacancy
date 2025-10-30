36b06041b1a34c8eef8fea54ff06a6752137990b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 16:45:13 -0300

    New endpoints TogglePrimary and toggle

--- JobVacancy.API/Controllers/EnterpriseIndustryController.cs
+++ JobVacancy.API/Controllers/EnterpriseIndustryController.cs
@@ -30,7 +30,7 @@ public class EnterpriseIndustryController(
     ) : Controller
 {
     [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ResponseHttp<string>))]
     [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseHttp<object>))]
     [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
@@ -105,10 +105,10 @@ public class EnterpriseIndustryController(
             dto.EnterpriseId = enterprise.Id;
             EnterpriseIndustryEntity entity = await enterpriseIndustryService.CreateAsync(dto);
 
-            return StatusCode(StatusCodes.Status201Created, new ResponseHttp<object>
+            return StatusCode(StatusCodes.Status201Created, new ResponseHttp<string>
             {
                 Code = StatusCodes.Status201Created,
-                Data = null,
+                Data = entity.Id,
                 Message = "Industry added with successfully",
                 Status = true,
                 Timestamp = DateTimeOffset.UtcNow,
@@ -271,6 +271,140 @@ public class EnterpriseIndustryController(
                 Timestamp = DateTimeOffset.UtcNow,
                 TraceId = HttpContext.TraceIdentifier,
                 Version = 1,
+            });
+        }
+        catch (Exception e) 
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+
+    [HttpPut("{id:required}/toggle/status/is-primary")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<string>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "ENTERPRISE_ROLE")]
+    public async Task<IActionResult> TogglePrimary(string id)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (userId == null) return Unauthorized();
+            
+            var entity = await enterpriseIndustryService.GetByIdAsync(id);
+            if (entity == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Data not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            entity.IsPrimary = !entity.IsPrimary;
+            await enterpriseIndustryService.UpdateSimple(entity);
+            
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status200OK,
+                Data = null,
+                Message = "Status is primary changed with successfully",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+            });
+        }
+        catch (Exception e) 
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+
+    [HttpPost("{industryId:required}/toggle")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ResponseHttp<string>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "ENTERPRISE_ROLE")]
+    public async Task<IActionResult> Toggle(string industryId)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (userId == null) return Unauthorized();
+            
+            EnterpriseEntity? enterprise = await enterpriseService.GetByUserId(userId);
+            if (enterprise == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Status = false,
+                    Message = "Enterprise not found",
+                    Data = null,
+                    Code = StatusCodes.Status404NotFound,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            EnterpriseIndustryEntity? entity = await enterpriseIndustryService.GetByIndustryIdAndEnterpriseId(industryId, enterprise.Id);
+            if (entity == null)
+            {
+                CreateEnterpriseIndustryDto dto = new CreateEnterpriseIndustryDto
+                {
+                    IndustryId = industryId,
+                    EnterpriseId = enterprise.Id,
+                };
+
+                EnterpriseIndustryEntity saved = await enterpriseIndustryService.CreateAsync(dto);
+
+                return StatusCode(StatusCodes.Status201Created, new ResponseHttp<string>
+                {
+                    Data = saved.Id,
+                    Status = true,
+                    Message = "Industry added with successfully",
+                    Code = 201,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1,
+                    Timestamp = DateTimeOffset.UtcNow,
+                });
+            }
+            
+            enterpriseIndustryService.Delete(entity);
+
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+            {
+                Data = null,
+                Status = true,
+                Message = "Industry removed with successfully",
+                Code = StatusCodes.Status200OK,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Timestamp = DateTimeOffset.UtcNow,
             });
         }
         catch (Exception e) 
