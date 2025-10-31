b63dfaeaf49e5e9f29bedd477c9f2c990b8a2ada
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-31 17:18:15 -0300

    New endpoint delete and getAll

--- JobVacancy.API/Controllers/PostUserController.cs
+++ JobVacancy.API/Controllers/PostUserController.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using JobVacancy.API.models.dtos.PostUser;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.PostUser;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -149,6 +151,114 @@ public class PostUserController(
                 TraceId = HttpContext.TraceIdentifier,
                 Version = 1
             });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpDelete("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (userId == null) return Unauthorized();
+            
+            PostUserEntity? post = await postUserService.GetById(id);
+            if (post == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Post not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            if (post.UserId != userId)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status403Forbidden,
+                    Message = "This post is not yours",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            await postUserService.Delete(post);
+            
+            return StatusCode(StatusCodes.Status204NoContent,new ResponseHttp<object>
+            {
+                Data = null,
+                Code = StatusCodes.Status204NoContent,
+                Message = "Post deleted with successfully.",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Page<PostUserDto>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> GetAll([FromQuery] PostUserFilterParam filter)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (userId == null) return Unauthorized();
+
+            var query = postUserService.Query();
+            IQueryable<PostUserEntity> appliedFilter = PostUserFilterQuery.ApplyFilter(query, filter);
+            
+            PaginatedList<PostUserEntity> paginatedList = await PaginatedList<PostUserEntity>.CreateAsync(
+                source: appliedFilter,
+                pageSize: filter.PageSize,
+                pageIndex: filter.PageNumber
+            );
+        
+            Page<PostUserDto> dtos = mapper.Map<Page<PostUserDto>>(paginatedList);
+        
+            return Ok(dtos);
         }
         catch (Exception e)
         {
