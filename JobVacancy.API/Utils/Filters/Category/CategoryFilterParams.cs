26063bd32d52c22abe779bce73e956f3fd402000
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-31 17:17:55 -0300

    Removed string.Empty; of Name and description

--- JobVacancy.API/Utils/Filters/Category/CategoryFilterParams.cs
+++ JobVacancy.API/Utils/Filters/Category/CategoryFilterParams.cs
@@ -2,7 +2,7 @@ namespace JobVacancy.API.Utils.Filters.Category;
 
 public class CategoryFilterParams: FilterBaseParams
 {
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public bool? IsActive { get; set; }
 }
