00e37039f927e78ccf9407b621eaf7e22aab1b79
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-31 17:50:40 -0300

    New column Category wity type CategoryDto

--- JobVacancy.API/models/dtos/BasePost.cs
+++ JobVacancy.API/models/dtos/BasePost.cs
@@ -1,3 +1,5 @@
+using JobVacancy.API.models.dtos.Category;
+
 namespace JobVacancy.API.models.dtos;
 
 public class BasePost: BaseDto
@@ -9,4 +11,5 @@ public class BasePost: BaseDto
     public bool IsFeatured { get; set; } = false;
     public int? ReadingTimeMinutes { get; set; }
     public string CategoryId { get; set; } = string.Empty;
+    public CategoryDto? Category { get; set; }
 }
