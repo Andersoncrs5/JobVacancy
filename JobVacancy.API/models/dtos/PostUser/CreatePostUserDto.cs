26b6fecee7ddd4e60aba792891687e151fed86aa
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-10-30 21:13:22 -0300

    Removed IsFeatured

--- JobVacancy.API/models/dtos/PostUser/CreatePostUserDto.cs
+++ JobVacancy.API/models/dtos/PostUser/CreatePostUserDto.cs
@@ -6,7 +6,6 @@ public class CreatePostUserDto
     public string Content { get; set; } = string.Empty;
     public string? ImageUrl { get; set; }
     public bool IsActive { get; set; }
-    public bool IsFeatured { get; set; }
     public int? ReadingTimeMinutes { get; set; }
     public string CategoryId { get; set; } = string.Empty;
     
