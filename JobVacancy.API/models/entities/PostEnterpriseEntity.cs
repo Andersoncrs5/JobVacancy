9e3e7df950145b40d6c326de846543dc65ab0b15
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-01 12:09:05 -0300

    Removed = new EnterpriseEntity(); of column Enterprise

--- JobVacancy.API/models/entities/PostEnterpriseEntity.cs
+++ JobVacancy.API/models/entities/PostEnterpriseEntity.cs
@@ -3,7 +3,7 @@ namespace JobVacancy.API.models.entities;
 public class PostEnterpriseEntity: BasePostTable
 {
     public string EnterpriseId { get; set; } = string.Empty;
-    public virtual EnterpriseEntity Enterprise { get; set; } = new EnterpriseEntity();
+    public virtual EnterpriseEntity? Enterprise { get; set; }
     public string CategoryId { get; set; } = string.Empty;
     public virtual CategoryEntity? Category { get; set; }
     
