7c6a68f90b9b94761d141b3fd57a8a9511f85422
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-01 20:31:59 -0300

    New endpoints

--- JobVacancy.API/Controllers/SkillController.cs
+++ JobVacancy.API/Controllers/SkillController.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using JobVacancy.API.models.dtos.Skill;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.Skill;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -22,6 +24,40 @@ public class SkillController(
     IMapper mapper
 ) : Controller
 {
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Page<SkillDto>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> GetAll([FromQuery] SkillFilterParam filter)
+    {
+        try
+        {
+            IQueryable<SkillEntity> iQueryable = skillService.Query();
+            IQueryable<SkillEntity> appliedFilter = SkillFilterQuery.ApplyFilter(iQueryable, filter);
+            PaginatedList<SkillEntity> paginatedList = await PaginatedList<SkillEntity>.CreateAsync(
+                source: appliedFilter,
+                pageSize: filter.PageSize,
+                pageIndex: filter.PageNumber
+            );
+        
+            Page<SkillDto> dtos = mapper.Map<Page<SkillDto>>(paginatedList);
+        
+            return Ok(dtos);
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<SkillDto>))]
     [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ResponseHttp<object>))]
@@ -52,7 +88,7 @@ public class SkillController(
             return StatusCode(StatusCodes.Status201Created, new ResponseHttp<SkillDto>
             {
                 Data = mapper.Map<SkillDto>(newSkill),
-                Message = "Category created with success.",
+                Message = "Skill created with success.",
                 Status = true,
                 Timestamp = DateTimeOffset.UtcNow,
                 TraceId = HttpContext.TraceIdentifier,
@@ -73,4 +109,216 @@ public class SkillController(
             });
         }
     }
+
+    [HttpGet("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<SkillDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> Get(string id)
+    {
+        try
+        {
+            SkillEntity? skill = await skillService.GetById(id);
+            if (skill == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Name is taken",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<SkillDto>
+            {
+                Data = mapper.Map<SkillDto>(skill),
+                Code = StatusCodes.Status200OK,
+                Message = "Skill found with success.",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpDelete("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
+    public async Task<IActionResult> Del(string id)
+    {
+        try
+        {
+            SkillEntity? skill = await skillService.GetById(id);
+            if (skill == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Skill not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            await skillService.Delete(skill);
+            
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+            {
+                Data = null,
+                Code = StatusCodes.Status200OK,
+                Message = "Skill deleted with success.",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpPatch("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<SkillDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
+    public async Task<IActionResult> Patch(string id, [FromBody] UpdateSkillDto dto)
+    {
+        try
+        {
+            SkillEntity? skill = await skillService.GetById(id);
+            if (skill == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Skill not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            if (!string.IsNullOrEmpty(dto.Name) && dto.Name != skill.Name)
+            {
+                bool checkName = await skillService.ExistsByName(dto.Name);
+                if (checkName)
+                {
+                    return StatusCode(StatusCodes.Status409Conflict, new ResponseHttp<object>
+                    {
+                        Code = StatusCodes.Status409Conflict,
+                        Message = "Name is taken",
+                        Data = null,
+                        Status = false,
+                        Timestamp = DateTimeOffset.UtcNow,
+                        TraceId = HttpContext.TraceIdentifier,
+                        Version = 1
+                    });
+                }
+                
+            }
+            
+            SkillEntity updated = await skillService.UpdateAsync(dto, skill);
+
+            SkillDto map = mapper.Map<SkillDto>(updated);
+
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<SkillDto>
+            {
+                Data = map,
+                Code = StatusCodes.Status200OK,
+                Message = "Skill updated with success.",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpGet("{name:required}/exists/name")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<bool>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
+    public async Task<IActionResult> ExistsByName(string name)
+    {
+        try
+        {
+            bool skill = await skillService.ExistsByName(name);
+            
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<bool>
+            {
+                Data = skill,
+                Code = StatusCodes.Status200OK,
+                Message = skill ? "Skill name exists" : "Skill name not exists",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
 }
