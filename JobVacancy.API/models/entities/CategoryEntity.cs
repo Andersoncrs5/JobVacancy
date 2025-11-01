5b05acc3869f667a846e901cbf2a8db41b611991
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-01 11:21:48 -0300

    New column PostsEnterprise

--- JobVacancy.API/models/entities/CategoryEntity.cs
+++ JobVacancy.API/models/entities/CategoryEntity.cs
@@ -6,4 +6,5 @@ public class CategoryEntity: BaseEntity
     public string? Description { get; set; }
     public bool IsActive { get; set; }
     public virtual ICollection<PostUserEntity> Posts { get; set; } = new List<PostUserEntity>();
+    public virtual ICollection<PostEnterpriseEntity> PostsEnterprise { get; set; } = new List<PostEnterpriseEntity>();
 }
