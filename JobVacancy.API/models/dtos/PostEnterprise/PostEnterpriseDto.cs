898042f3d89bcd78712e767c8d7762ebe4ec93fd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-01 12:08:48 -0300

    New column EnterpriseId

--- JobVacancy.API/models/dtos/PostEnterprise/PostEnterpriseDto.cs
+++ JobVacancy.API/models/dtos/PostEnterprise/PostEnterpriseDto.cs
@@ -4,5 +4,6 @@ namespace JobVacancy.API.models.dtos.PostEnterprise;
 
 public class PostEnterpriseDto: BasePost
 {
+    public string EnterpriseId { get; set; } = string.Empty; 
     public EnterpriseDto? Enterprise { get; set; }
 }
