39230bc64e9398b66d7c1dd82901776cbf689f61
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-01 21:11:08 -0300

    New method GetByUserIdAndSkillIdWithEntity

--- JobVacancy.API/Repositories/Provider/UserSkillRepository.cs
+++ JobVacancy.API/Repositories/Provider/UserSkillRepository.cs
@@ -18,5 +18,15 @@ public class UserSkillRepository(AppDbContext context)
         return await context.UserSkill.FirstOrDefaultAsync(e => e.UserId == userId && e.SkillId == skillId);
     }
     
+    public async Task<UserSkillEntity?> GetByUserIdAndSkillIdWithEntity(string userId, string skillId)
+    {
+        IQueryable<UserSkillEntity> query = context.UserSkill.AsQueryable();
+        query = query.Where(e => e.UserId == userId && e.SkillId == skillId);
+        
+        query = query.Include(e => e.Skill);
+        query = query.Include(e => e.User);
+        
+        return await query.FirstOrDefaultAsync();
+    }
     
 }
