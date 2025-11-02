7429dea3790456609153dbd7d2a8733f37e86ed0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-02 15:34:07 -0300

    New table FavoritePostUserEntity

---
+++ JobVacancy.API/models/entities/FavoritePostUserEntity.cs
@@ -0,0 +1,11 @@
+namespace JobVacancy.API.models.entities;
+
+public class FavoritePostUserEntity: BaseEntity
+{
+    public string UserId { get; set; } = string.Empty;
+    public virtual UserEntity? User { get; set; }
+    
+    public string PostUserId { get; set; } = string.Empty;
+    public virtual PostUserEntity? PostUser { get; set; }
+    
+}
