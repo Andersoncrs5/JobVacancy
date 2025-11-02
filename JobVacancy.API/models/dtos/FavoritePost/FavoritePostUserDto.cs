d1d5148e1a9fd19815d2a199f62403fd104aee0f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-02 17:09:41 -0300

    Change type columns user and PostUser

--- JobVacancy.API/models/dtos/FavoritePost/FavoritePostUserDto.cs
+++ JobVacancy.API/models/dtos/FavoritePost/FavoritePostUserDto.cs
@@ -1,14 +1,15 @@
-using JobVacancy.API.models.entities;
+using JobVacancy.API.models.dtos.PostUser;
+using JobVacancy.API.models.dtos.Users;
 
 namespace JobVacancy.API.models.dtos.FavoritePost;
 
-public class FavoritePostDto
+public class FavoritePostUserDto: BaseDto
 {
     public string UserId { get; set; } = string.Empty;
-    public virtual UserEntity? User { get; set; }
+    public virtual UserDto? User { get; set; }
     
     public string PostUserId { get; set; } = string.Empty;
-    public virtual PostUserEntity? PostUser { get; set; }
+    public virtual PostUserDto? PostUser { get; set; }
     
     public string? UserNotes { get; set; }
     public int? UserRating { get; set; }
