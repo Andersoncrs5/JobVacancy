87e4450602dfa67c779136c05bbec08fb6aca036
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-03 20:15:51 -0300

    New base CommentBaseEntity

---
+++ JobVacancy.API/models/entities/Base/CommentBaseEntity.cs
@@ -0,0 +1,16 @@
+namespace JobVacancy.API.models.entities.Base;
+
+public class CommentBaseEntity: BaseEntity
+{
+    public string Content { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public string? ImageUrl { get; set; }
+    public int? Depth { get; set; }
+    public string? ParentCommentId { get; set; }
+
+    public string UserId { get; set; } = string.Empty;
+
+    public virtual UserEntity? User { get; set; }
+    public virtual CommentBaseEntity? ParentComment { get; set; }
+    public virtual ICollection<CommentBaseEntity> Replies { get; set; } = new List<CommentBaseEntity>();
+}
