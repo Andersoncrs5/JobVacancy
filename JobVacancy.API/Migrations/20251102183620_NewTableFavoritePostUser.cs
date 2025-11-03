3a8ac6ad188c5c845efeaef6d1521171efd5b9f4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-03 20:34:09 -0300

    New migrations

---
+++ JobVacancy.API/Migrations/20251102183620_NewTableFavoritePostUser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobVacancy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTableFavoritePostUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FavoritePostUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    PostUserId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FavoritePostUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FavoritePostUser_PostUsers_PostUserId",
+                        column: x => x.PostUserId,
+                        principalTable: "PostUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FavoritePostUser_app_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "app_users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritePostUser_PostUserId",
+                table: "FavoritePostUser",
+                column: "PostUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritePostUser_UserId",
+                table: "FavoritePostUser",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritePostUser_UserId_PostUserId",
+                table: "FavoritePostUser",
+                columns: new[] { "UserId", "PostUserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FavoritePostUser");
+        }
+    }
+}
---
+++ JobVacancy.API/Migrations/20251102184024_NewColumnsToFavoritePostUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobVacancy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewColumnsToFavoritePostUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserNotes",
+                table: "FavoritePostUser",
+                type: "character varying(600)",
+                maxLength: 600,
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "UserRating",
+                table: "FavoritePostUser",
+                type: "SMALLINT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserNotes",
+                table: "FavoritePostUser");
+
+            migrationBuilder.DropColumn(
+                name: "UserRating",
+                table: "FavoritePostUser");
+        }
+    }
+}
