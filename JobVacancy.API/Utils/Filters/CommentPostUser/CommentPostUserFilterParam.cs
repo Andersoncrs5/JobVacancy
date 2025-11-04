c9540b288698a7937e91929906e4ea3d66e542e8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-04 14:11:50 -0300

    Setted CommentPostUserFilterParam

---
+++ JobVacancy.API/Utils/Filters/CommentPostUser/CommentPostUserFilterParam.cs
@@ -0,0 +1,12 @@
+using JobVacancy.API.Utils.Filters.Base;
+
+namespace JobVacancy.API.Utils.Filters.CommentPostUser;
+
+public class CommentPostUserFilterParam: CommentBaseFilterParams
+{
+    public string? CategoryId { get; set; }
+    public string? Title { get; set; }
+    public string? ContentPost { get; set; }
+    public int? ReadingTimeMinutesMin { get; set; }
+    public int? ReadingTimeMinutesMax { get; set; }
+}
