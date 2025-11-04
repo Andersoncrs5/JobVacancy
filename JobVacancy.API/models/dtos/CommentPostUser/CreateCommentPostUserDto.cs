838fa064142ced922635207e54e34ebdaac6ce8e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-04 14:12:47 -0300

    New params

--- JobVacancy.API/models/dtos/CommentPostUser/CreateCommentPostUserDto.cs
+++ JobVacancy.API/models/dtos/CommentPostUser/CreateCommentPostUserDto.cs
@@ -2,5 +2,9 @@ namespace JobVacancy.API.models.dtos.CommentPostUser;
 
 public class CreateCommentPostUserDto
 {
-    
+    public string Content { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public string? ImageUrl { get; set; }
+    public int? Depth { get; set; }
+    public string PostId { get; set; }
 }
