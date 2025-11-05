2fa9b71790a0a0fa27952bc34ce23cef5e78d6ca
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-05 10:49:24 -0300

    Impled ICommentPostEnterpriseService interface and provider

---
+++ JobVacancy.API/Services/Providers/CommentPostEnterpriseService.cs
@@ -0,0 +1,9 @@
+using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Uow.Interfaces;
+
+namespace JobVacancy.API.Services.Providers;
+
+public class CommentPostEnterpriseService(IUnitOfWork uow): ICommentPostEnterpriseService
+{
+    
+}
