f6a33fd76bd1709d58319476219625f663b53b17
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-05 09:38:19 -0300

    Configured relationship between table CommentPostEnterprise

--- JobVacancy.API/models/entities/PostEnterpriseEntity.cs
+++ JobVacancy.API/models/entities/PostEnterpriseEntity.cs
@@ -7,4 +7,6 @@ public class PostEnterpriseEntity: BasePostTable
     public string CategoryId { get; set; } = string.Empty;
     public virtual CategoryEntity? Category { get; set; }
     public virtual ICollection<FavoritePostEnterpriseEntity> FavoritePostsEnterprise { get; set; } = new List<FavoritePostEnterpriseEntity>();
+    public virtual ICollection<CommentPostEnterpriseEntity> Comments { get; set; } = new List<CommentPostEnterpriseEntity>();
+    
 }
