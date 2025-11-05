330b8d0921e5eb7a2f097652c7e72d0852fb8fc7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-05 16:42:28 -0300

    New CommentPostEnterpriseFilterParam

---
+++ JobVacancy.API/Utils/Filters/CommentPostEnterprise/CommentPostEnterpriseFilterParam.cs
@@ -0,0 +1,20 @@
+using JobVacancy.API.models.entities.Enums;
+using JobVacancy.API.Utils.Filters.Base;
+
+namespace JobVacancy.API.Utils.Filters.CommentPostEnterprise;
+
+public class CommentPostEnterpriseFilterParam: CommentBaseFilterParams
+{
+    public string? CategoryId { get; set; }
+    
+    public string? EnterpriseId { get; set; }
+    public string? NameEnterprise { get; set; }
+    public EnterpriseTypeEnum? Type  { get; set; }
+    
+    public string? Title { get; set; }
+    public string? ContentPost { get; set; }
+    public int? ReadingTimeMinutesMin { get; set; }
+    public int? ReadingTimeMinutesMax { get; set; }
+    
+    
+}
