5220c2b612e5e9a7c8bc5af48268bf7cab614847
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-05 12:17:23 -0300

    Impled methods

--- JobVacancy.API/Services/Interfaces/ICommentPostEnterpriseService.cs
+++ JobVacancy.API/Services/Interfaces/ICommentPostEnterpriseService.cs
@@ -1,6 +1,14 @@
+using JobVacancy.API.models.dtos.CommentPostEnterprise;
+using JobVacancy.API.models.entities;
+
 namespace JobVacancy.API.Services.Interfaces;
 
 public interface ICommentPostEnterpriseService
 {
-    
+    Task<CommentPostEnterpriseEntity?> GetById(string id);
+    Task<bool> ExistsById(string id);
+    Task Delete(CommentPostEnterpriseEntity entity);
+    Task<CommentPostEnterpriseEntity> Create(CreateCommentPostEnterpriseDto dto, string userId,string? parentId);
+    IQueryable<CommentPostEnterpriseEntity> Query();
+    Task<CommentPostEnterpriseEntity> Update(CommentPostEnterpriseEntity comment, UpdateCommentPostEnterpriseDto dto);
 }
--- JobVacancy.API/Services/Providers/CommentPostEnterpriseService.cs
+++ JobVacancy.API/Services/Providers/CommentPostEnterpriseService.cs
@@ -1,9 +1,64 @@
+using AutoMapper;
+using JobVacancy.API.models.dtos.CommentPostEnterprise;
+using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
 using JobVacancy.API.Utils.Uow.Interfaces;
 
 namespace JobVacancy.API.Services.Providers;
 
-public class CommentPostEnterpriseService(IUnitOfWork uow): ICommentPostEnterpriseService
+public class CommentPostEnterpriseService(IUnitOfWork uow, IMapper mapper): ICommentPostEnterpriseService
 {
+    public async Task<CommentPostEnterpriseEntity?> GetById(string id)
+    {
+        return await uow.CommentPostEnterpriseRepository.GetByIdAsync(id);
+    }
+    
+    public async Task<bool> ExistsById(string id)
+    {
+        return await uow.CommentPostEnterpriseRepository.ExistsById(id);
+    }
+
+    public async Task Delete(CommentPostEnterpriseEntity entity)
+    {
+        uow.CommentPostEnterpriseRepository.Delete(entity);
+        await uow.Commit();
+    }
+
+    public async Task<CommentPostEnterpriseEntity> Create(CreateCommentPostEnterpriseDto dto, string userId,
+        string? parentId)
+    {
+        CommentPostEnterpriseEntity map = mapper.Map<CommentPostEnterpriseEntity>(dto);
+        map.UserId = userId;
+        map.ParentCommentId = parentId;
+
+        CommentPostEnterpriseEntity entity = await uow.CommentPostEnterpriseRepository.AddAsync(map);
+        await uow.Commit();
+        return entity;
+    }
+
+    public IQueryable<CommentPostEnterpriseEntity> Query()
+    {
+        return uow.CommentPostEnterpriseRepository.ReturnIQueryable();
+    }
+
+    public async Task<CommentPostEnterpriseEntity> Update(CommentPostEnterpriseEntity comment, UpdateCommentPostEnterpriseDto dto)
+    {
+        bool isActive = comment.IsActive;
+        
+        mapper.Map(dto, comment);
+
+        if (dto.IsActive != null)
+        {
+            comment.IsActive = dto.IsActive.Value;
+        }
+        else
+        {
+            comment.IsActive = isActive;
+        }
+        
+        CommentPostEnterpriseEntity updated = await uow.CommentPostEnterpriseRepository.Update(comment);
+        await uow.Commit();
+        return updated;
+    }
     
 }
