f49ee7dcc1b9a0b4642fbfa6349275d9242a54a8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 14:22:05 -0300

    New EmployeeInvitationEntity

---
+++ JobVacancy.API/models/entities/EmployeeInvitationEntity.cs
@@ -0,0 +1,28 @@
+using JobVacancy.API.models.entities.Enums;
+
+namespace JobVacancy.API.models.entities;
+
+public class EmployeeInvitationEntity: BaseEntity
+{
+    public string UserId {get; set;}
+    public UserEntity? User {get; set;}
+    
+    public string EnterpriseId {get; set;}
+    public EnterpriseEntity? Enterprise {get; set;}
+    
+    public string? Message {get; set;} 
+    public string? RejectReason {get; set;} 
+    public string? InvitationLink {get; set;} 
+    public string? Token {get; set;} 
+    public string Position {get; set;} = string.Empty;
+    public string SalaryRange {get; set;} = string.Empty;
+    public EmploymentTypeEnum EmploymentType {get; set;}
+    public DateTime ProposedStartDate {  get; set; }
+    public DateTime? ProposedEndDate { get; set; }
+    public StatusEnum Status { get; set; }
+    public DateTime? ResponseDate { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    
+    public string? InviteSenderId  {get; set;}
+    public UserEntity? InviteSender {get; set;}
+}
