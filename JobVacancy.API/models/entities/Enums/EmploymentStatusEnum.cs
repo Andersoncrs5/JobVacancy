ecc70a0d6c1fe7e9dfb35cef22175593e163113a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 14:12:36 -0300

    New EmploymentStatusEnum

---
+++ JobVacancy.API/models/entities/Enums/EmploymentStatusEnum.cs
@@ -0,0 +1,10 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum EmploymentStatusEnum
+{
+    CurrentEmployee = 1,
+    FormerEmployee = 2,
+    OnLeave = 3,   
+    Vacation = 4,   
+    Probation = 5, 
+}
