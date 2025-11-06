9a6ce3ff36dd666bf3952003946c0efda6fec311
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 14:12:43 -0300

    New EmploymentStatusEnum

---
+++ JobVacancy.API/models/entities/Enums/EmploymentTypeEnum.cs
@@ -0,0 +1,13 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum EmploymentTypeEnum
+{
+    FullTime = 1,
+    PartTime = 2,
+    Internship = 3,
+    Contract = 4,
+    Temporary = 5,
+    Freelance = 6,
+    Apprentice = 7,
+    Seasonal =  8,
+}
