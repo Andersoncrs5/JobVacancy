444e428f6b192318952d53f0bf8f03ec5c84876f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 14:21:32 -0300

    New CurrencyEnum

---
+++ JobVacancy.API/models/entities/Enums/CurrencyEnum.cs
@@ -0,0 +1,23 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum CurrencyEnum
+{
+    Undefined = 0,
+    Usd = 1, 
+    Eur = 2, 
+    Jpy = 3, 
+    Gbp = 4, 
+    Aud = 5, 
+    Cad = 6, 
+    Chf = 7, 
+    Cny = 8, 
+    Inr = 9, 
+    Brl = 10, 
+    Rub = 11, 
+    Mxn = 12, 
+    Zar = 13, 
+    Nzd = 14,
+    Dkk = 15, 
+    Hkd = 16,
+    Mkd = 17,
+}
