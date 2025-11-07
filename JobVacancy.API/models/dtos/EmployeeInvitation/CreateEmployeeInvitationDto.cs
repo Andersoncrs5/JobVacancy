509bb198806ab803decf2057b29ceebf9ceecc05
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 21:38:28 -0300

    Little Improvements

--- JobVacancy.API/models/dtos/EmployeeInvitation/CreateEmployeeInvitationDto.cs
+++ JobVacancy.API/models/dtos/EmployeeInvitation/CreateEmployeeInvitationDto.cs
@@ -4,13 +4,12 @@ namespace JobVacancy.API.models.dtos.EmployeeInvitation;
 
 public class CreateEmployeeInvitationDto
 {
-    public string UserId {get; set;}
+    public string UserId {get; set;} = string.Empty;
     public string? Message {get; set;}
     public string Position {get; set;} = string.Empty;
     public string SalaryRange {get; set;} = string.Empty;
     public EmploymentTypeEnum EmploymentType {get; set;}
     public DateTime ProposedStartDate {  get; set; }
     public DateTime? ProposedEndDate { get; set; }
-    public StatusEnum Status { get; set; }
     public CurrencyEnum Currency { get; set; }
 }
