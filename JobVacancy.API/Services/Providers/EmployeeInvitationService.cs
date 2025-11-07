d9596b007561e043171b181c056bcdd227e18e0f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-07 20:41:35 -0300

    Remake method Update and new method UpdateByUser

--- JobVacancy.API/Services/Providers/EmployeeInvitationService.cs
+++ JobVacancy.API/Services/Providers/EmployeeInvitationService.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using JobVacancy.API.models.dtos.EmployeeInvitation;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.models.entities.Enums;
@@ -6,7 +7,7 @@ using JobVacancy.API.Utils.Uow.Interfaces;
 
 namespace JobVacancy.API.Services.Providers;
 
-public class EmployeeInvitationService(IUnitOfWork uow): IEmployeeInvitationService
+public class EmployeeInvitationService(IUnitOfWork uow, IMapper mapper): IEmployeeInvitationService
 {
     public async Task<EmployeeInvitationEntity?> GetById(string id)
     {
@@ -27,6 +28,7 @@ public class EmployeeInvitationService(IUnitOfWork uow): IEmployeeInvitationServ
     public async Task<EmployeeInvitationEntity> Create(CreateEmployeeInvitationDto dto, string enterpriseId,
         string? inviteSenderId)
     {
+        
         EmployeeInvitationEntity map = uow.Mapper.Map<EmployeeInvitationEntity>(dto);
         map.InviteSenderId = inviteSenderId;
         map.EnterpriseId = enterpriseId;
@@ -34,15 +36,11 @@ public class EmployeeInvitationService(IUnitOfWork uow): IEmployeeInvitationServ
     
         map.ExpiresAt = DateTime.UtcNow.AddDays(7); 
         
-        if (map.ProposedEndDate.HasValue && map.ProposedEndDate.Value < map.ProposedStartDate.AddDays(10))
+        if (map.ProposedEndDate.HasValue)
         {
-            map.ExpiresAt = DateTime.UtcNow.AddDays(3);
+            map.ExpiresAt = DateTime.UtcNow.AddDays(map.ProposedEndDate.Value.Day + 15);
         }
-        else
-        {
-            map.ExpiresAt = DateTime.UtcNow.AddDays(30); 
-        }
-    
+        
         map.ResponseDate = null; 
 
         EmployeeInvitationEntity created = await uow.EmployeeInvitationRepository.AddAsync(map);
@@ -53,8 +51,66 @@ public class EmployeeInvitationService(IUnitOfWork uow): IEmployeeInvitationServ
     public async Task<EmployeeInvitationEntity> Update(UpdateEmployeeInvitationDto dto,
         EmployeeInvitationEntity invitation)
     {
-        uow.Mapper.Map(dto, invitation);
+        if (!string.IsNullOrEmpty(dto.Message))
+        {
+            invitation.Message = dto.Message;
+        }
+        
+        if (!string.IsNullOrEmpty(dto.Position))
+        {
+            invitation.Position = dto.Position;
+        }
+        
+        if (!string.IsNullOrEmpty(dto.SalaryRange))
+        {
+            invitation.SalaryRange = dto.SalaryRange;
+        }
+        
+        if (dto.EmploymentType.HasValue)
+        {
+            invitation.EmploymentType = dto.EmploymentType.Value;
+        }
+        
+        if (dto.ProposedStartDate.HasValue && dto.ProposedStartDate != invitation.ProposedStartDate)
+        {
+            invitation.ProposedStartDate = dto.ProposedStartDate.Value;
+        }
+        
+        if (dto.ProposedEndDate.HasValue && dto.ProposedEndDate != invitation.ProposedEndDate)
+        {
+            invitation.ProposedEndDate = dto.ProposedEndDate.Value;
+        }
+        
+        if (dto.Currency.HasValue)
+        {
+            invitation.Currency = dto.Currency.Value;
+        }
+        
+        if (dto.Currency.HasValue)
+        {
+            invitation.Currency = dto.Currency.Value;
+        }
+        
+        EmployeeInvitationEntity update = await uow.EmployeeInvitationRepository.Update(invitation);
+        await uow.Commit();
+        return invitation;
+    }
 
+    public async Task<EmployeeInvitationEntity> UpdateByUser(UpdateEmployeeInvitationByUserDto dto,
+        EmployeeInvitationEntity invitation)
+    {
+        if (!string.IsNullOrEmpty(dto.RejectReason))
+        {
+            invitation.RejectReason = dto.RejectReason;
+        }
+        
+        if (dto.Status.HasValue)
+        {
+            invitation.Status = dto.Status.Value;
+        }
+
+        invitation.ResponseDate = DateTime.UtcNow;
+        
         EmployeeInvitationEntity update = await uow.EmployeeInvitationRepository.Update(invitation);
         await uow.Commit();
         return invitation;
