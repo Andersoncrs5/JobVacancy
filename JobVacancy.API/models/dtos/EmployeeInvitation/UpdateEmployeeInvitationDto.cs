9e4b8d9539162d18831b0c2ea1d1156996db93a5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-06 21:40:29 -0300

    Removed Status

--- JobVacancy.API/models/dtos/EmployeeInvitation/UpdateEmployeeInvitationDto.cs
+++ JobVacancy.API/models/dtos/EmployeeInvitation/UpdateEmployeeInvitationDto.cs
@@ -4,13 +4,11 @@ namespace JobVacancy.API.models.dtos.EmployeeInvitation;
 
 public class UpdateEmployeeInvitationDto
 {
-    public string? UserId {get; set;} 
     public string? Message {get; set;}
     public string? Position {get; set;}
     public string? SalaryRange {get; set;}
     public EmploymentTypeEnum? EmploymentType {get; set;}
     public DateTime? ProposedStartDate {  get; set; }
     public DateTime? ProposedEndDate { get; set; }
-    public StatusEnum? Status { get; set; }
     public CurrencyEnum? Currency { get; set; }
 }
