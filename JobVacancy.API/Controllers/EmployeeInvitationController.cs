b4eddbe466d66460c2a3e3016c0d87f5d2b41cf7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-07 20:37:12 -0300

    New endpoint UpdateByUser

--- JobVacancy.API/Controllers/EmployeeInvitationController.cs
+++ JobVacancy.API/Controllers/EmployeeInvitationController.cs
@@ -199,7 +199,6 @@ public class EmployeeInvitationController(
         }
     }
     
-    
     [HttpPatch("{id:required}/By/Enterprise")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<EmployeeInvitationDto>))]
     [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
@@ -240,6 +239,87 @@ public class EmployeeInvitationController(
             }
 
             EmployeeInvitationEntity update = await employeeInvitationService.Update(dto, employee);
+
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<EmployeeInvitationDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Data = mapper.Map<EmployeeInvitationDto>(update),
+                Message = "Invitation updated",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpPatch("{id:required}/By/User")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<EmployeeInvitationDto>))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> UpdateByUser(string id, [FromBody] UpdateEmployeeInvitationByUserDto dto)
+    {
+        try
+        {
+            EmployeeInvitationEntity? employee = await employeeInvitationService.GetById(id);
+            if (employee == null) 
+            {
+                return NotFound(new ResponseHttp<object>
+                {
+                    Code = 404,
+                    Message = "Employee Not Found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            if (employee.Status == StatusEnum.Accepted) 
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+                {
+                    Data = null,
+                    Code = StatusCodes.Status403Forbidden,
+                    Message = $"This invitation cannot be updated",
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            if (employee.ExpiresAt < DateTime.UtcNow)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+                {
+                    Status = false,
+                    Code = StatusCodes.Status403Forbidden,
+                    Data = null,
+                    Message = $"This invitation is expired",
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            EmployeeInvitationEntity update = await employeeInvitationService.UpdateByUser(dto, employee);
 
             return StatusCode(StatusCodes.Status200OK, new ResponseHttp<EmployeeInvitationDto>
             {
