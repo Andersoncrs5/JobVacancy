8f8dbb8b75b79cb1e278c5197dfe5f0e84257272
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 16:26:19 -0300

    New method ExistsByUserIdAndEnterpriseId

--- JobVacancy.API/Repositories/Interfaces/IEmployeeEnterpriseRepository.cs
+++ JobVacancy.API/Repositories/Interfaces/IEmployeeEnterpriseRepository.cs
@@ -4,5 +4,5 @@ namespace JobVacancy.API.Repositories.Interfaces;
 
 public interface IEmployeeEnterpriseRepository: IGenericRepository<EmployeeEnterpriseEntity>
 {
-    
+    Task<bool> ExistsByUserIdAndEnterpriseId(string userId, string enterpriseId);
 }
--- JobVacancy.API/Repositories/Provider/EmployeeEnterpriseRepository.cs
+++ JobVacancy.API/Repositories/Provider/EmployeeEnterpriseRepository.cs
@@ -1,10 +1,14 @@
 using JobVacancy.API.Context;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobVacancy.API.Repositories.Provider;
 
 public class EmployeeEnterpriseRepository(AppDbContext context): GenericRepository<EmployeeEnterpriseEntity>(context), IEmployeeEnterpriseRepository
 {
-    
+    public async Task<bool> ExistsByUserIdAndEnterpriseId(string userId, string enterpriseId)
+    {
+        return await context.EmployeeEnterprises.AnyAsync(x => x.UserId == userId && x.EnterpriseId == enterpriseId);
+    }
 }
