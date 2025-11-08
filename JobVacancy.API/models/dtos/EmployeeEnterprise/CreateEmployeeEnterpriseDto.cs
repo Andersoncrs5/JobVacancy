cd1a5856b262d5c65a05d4a46570d17a3136bccf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 16:06:46 -0300

    new CreateEmployeeEnterpriseDto

---
+++ JobVacancy.API/models/dtos/EmployeeEnterprise/CreateEmployeeEnterpriseDto.cs
@@ -0,0 +1,24 @@
+using JobVacancy.API.models.entities.Enums;
+
+namespace JobVacancy.API.models.dtos.EmployeeEnterprise;
+
+public class CreateEmployeeEnterpriseDto
+{
+    public string UserId {get; set;} = string.Empty;
+    public string? ContractLink {get; set;}
+    public string SalaryRange {get; set;} = string.Empty;
+    public string? TerminationReason {get; set;} 
+    public string? Notes {get; set;}
+    public decimal SalaryValue {get; set;}
+    public PaymentFrequencyEnum PaymentFrequency {get; set;}
+    public ContractLegalTypeEnum? ContractLegalType {get; set;}
+    public EmploymentTypeEnum ContractType {get; set;} 
+    public CurrencyEnum SalaryCurrency { get; set; } 
+    public EmploymentTypeEnum EmploymentType {get; set;}
+    public EmploymentStatusEnum EmploymentStatus {get; set;}
+    public CurrencyEnum Currency { get; set; }
+    public DateTime StartDate {get; set;}
+    public DateTime? EndDate {get; set;}
+    public string PositionId {get; set;} = string.Empty;
+    public string? InviteSenderId  {get; set;}
+}
