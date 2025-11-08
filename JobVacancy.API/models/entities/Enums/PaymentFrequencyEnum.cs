1d1d762c070a469893c9f6b321876fd408cfa4c2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 15:47:00 -0300

    New PaymentFrequencyEnum

---
+++ JobVacancy.API/models/entities/Enums/PaymentFrequencyEnum.cs
@@ -0,0 +1,15 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum PaymentFrequencyEnum
+{
+    None,
+    OneTime,
+    Daily,
+    Weekly,
+    BiWeekly,
+    Monthly,
+    BiMonthly,
+    Quarterly,
+    SemiAnnually,
+    Annually
+}
