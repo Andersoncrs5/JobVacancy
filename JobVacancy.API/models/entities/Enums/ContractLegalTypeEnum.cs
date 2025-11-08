3c181b32da2087811a501358da7c4c997af582cd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 15:19:54 -0300

    New ContractLegalTypeEnum

---
+++ JobVacancy.API/models/entities/Enums/ContractLegalTypeEnum.cs
@@ -0,0 +1,12 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum ContractLegalTypeEnum
+{
+    Indefinite = 1,
+    FixedTerm = 2,
+    LegalEntity = 3,
+    Internship = 4,
+    Freelancer = 5,
+    Outsourced = 6,
+    Intermittent = 7
+}
