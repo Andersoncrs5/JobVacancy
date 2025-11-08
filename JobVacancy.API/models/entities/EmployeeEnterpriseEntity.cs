cef5899625ea07540258e1a8c860ac34694dca86
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 15:46:38 -0300

    new column PaymentFrequency

--- JobVacancy.API/models/entities/EmployeeEnterpriseEntity.cs
+++ JobVacancy.API/models/entities/EmployeeEnterpriseEntity.cs
@@ -5,18 +5,19 @@ namespace JobVacancy.API.models.entities;
 public class EmployeeEnterpriseEntity: BaseEntity
 {
     public string UserId {get; set;}
-    public UserEntity? User {get; set;}
+    public virtual UserEntity? User {get; set;}
     
     public string EnterpriseId {get; set;}
-    public EnterpriseEntity? Enterprise {get; set;}
+    public virtual EnterpriseEntity? Enterprise {get; set;}
     
     public string? ContractLink {get; set;}
     public string SalaryRange {get; set;} = string.Empty;
     public string? TerminationReason {get; set;} 
     public string? Notes {get; set;}
     
-    public decimal? SalaryValue {get; set;}
+    public decimal SalaryValue {get; set;}
 
+    public PaymentFrequencyEnum PaymentFrequency {get; set;}
     public ContractLegalTypeEnum? ContractLegalType {get; set;}
     public EmploymentTypeEnum ContractType {get; set;} 
     public CurrencyEnum SalaryCurrency { get; set; } 
