528b9b97e46942807e93596176a745a6eb97eef0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-08 15:20:03 -0300

    New columns

--- JobVacancy.API/models/entities/EmployeeEnterpriseEntity.cs
+++ JobVacancy.API/models/entities/EmployeeEnterpriseEntity.cs
@@ -10,12 +10,26 @@ public class EmployeeEnterpriseEntity: BaseEntity
     public string EnterpriseId {get; set;}
     public EnterpriseEntity? Enterprise {get; set;}
     
-    public string Position {get; set;} = string.Empty;
+    public string? ContractLink {get; set;}
     public string SalaryRange {get; set;} = string.Empty;
+    public string? TerminationReason {get; set;} 
+    public string? Notes {get; set;}
     
+    public decimal? SalaryValue {get; set;}
+
+    public ContractLegalTypeEnum? ContractLegalType {get; set;}
+    public EmploymentTypeEnum ContractType {get; set;} 
+    public CurrencyEnum SalaryCurrency { get; set; } 
     public EmploymentTypeEnum EmploymentType {get; set;}
     public EmploymentStatusEnum EmploymentStatus {get; set;}
+    public CurrencyEnum Currency { get; set; }
     
     public DateTime StartDate {get; set;}
-    public DateTime EndDate {get; set;}
+    public DateTime? EndDate {get; set;}
+    
+    public string PositionId {get; set;} = string.Empty;
+    public virtual PositionEntity? Position { get; set; }
+    
+    public string? InviteSenderId  {get; set;}
+    public virtual UserEntity? InviteSender {get; set;}
 }
