38c947f5cff40a8ffa26ed1f9b31cb8da8d539a1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-09 14:17:16 -0300

    New endpoints

--- JobVacancy.API/Controllers/EmployeeEnterpriseController.cs
+++ JobVacancy.API/Controllers/EmployeeEnterpriseController.cs
@@ -16,11 +16,13 @@ namespace JobVacancy.API.Controllers;
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
 [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+// colocar check para saber se o user ja esta cadastrado na levando em conta a endDate
 public class EmployeeEnterpriseController(
     IEnterpriseService enterpriseService,
     IUserService userService,
     IEmployeeInvitationService employeeInvitationService,
     IEmployeeEnterpriseService  employeeEnterpriseService,
+    IPositionService positionService,
     IConfiguration configuration,
     IMapper mapper
 ) : Controller {
@@ -97,10 +99,27 @@ public class EmployeeEnterpriseController(
                     TraceId = HttpContext.TraceIdentifier,
                 });
             }
-            
+
+            EmployeeEnterpriseEntity? exists = await employeeEnterpriseService.GetByUserIdAndEnterpriseId(invitation.UserId, enterprise.Id);
+            if (exists != null && exists.EndDate == null)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status409Conflict,
+                    Message = "User already is registered",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
             dto.InviteSenderId = invitation.InviteSenderId;
             
             EmployeeEnterpriseEntity map = await employeeEnterpriseService.Create(dto, enterprise.Id,  invitation.UserId);
+            
+            await employeeInvitationService.Delete(invitation);
 
             return StatusCode(StatusCodes.Status201Created, new ResponseHttp<EmployeeEnterpriseDto>
             {
@@ -127,4 +146,189 @@ public class EmployeeEnterpriseController(
             });
         }
     }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<EmployeeEnterpriseDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> Get(string id)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+            
+            EmployeeEnterpriseEntity? exists = await employeeEnterpriseService.GetById(id);
+            if (exists == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Resource not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<EmployeeEnterpriseDto>
+            {
+                Code = StatusCodes.Status200OK,
+                Data = mapper.Map<EmployeeEnterpriseDto>(exists),
+                Message = "Resource found",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "ENTERPRISE_ROLE")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+            
+            EmployeeEnterpriseEntity? exists = await employeeEnterpriseService.GetById(id);
+            if (exists == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Resource not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            await employeeEnterpriseService.Delete(exists);
+            
+            return StatusCode(StatusCodes.Status204NoContent, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status204NoContent,
+                Data = null,
+                Message = "User removed",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<EmployeeEnterpriseDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ResponseHttp<object>))]
+    [Authorize(Roles = "ENTERPRISE_ROLE")]
+    public async Task<IActionResult> Patch(string id, [FromBody] UpdateEmployeeEnterpriseDto dto) 
+    {
+        try
+        {
+            string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+            if (string.IsNullOrEmpty(userId)) return Unauthorized();
+            
+            EmployeeEnterpriseEntity? exists = await employeeEnterpriseService.GetById(id);
+            if (exists == null) 
+            {
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Resource not found",
+                    Data = null,
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+
+            if (
+                !string.IsNullOrEmpty(dto.PositionId) && 
+                dto.PositionId != exists.PositionId &&
+                !await positionService.ExistsById(dto.PositionId)
+                ) 
+            {
+                
+                return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+                {
+                    Data = null,
+                    Code = StatusCodes.Status404NotFound,
+                    Message = "Position not found",
+                    Status = false,
+                    Timestamp = DateTimeOffset.UtcNow,
+                    TraceId = HttpContext.TraceIdentifier,
+                    Version = 1
+                });
+            }
+            
+            EmployeeEnterpriseEntity entity = await employeeEnterpriseService.Update(dto, exists);
+
+            return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status200OK,
+                Data = mapper.Map<EmployeeEnterpriseDto>(entity),
+                Message = "User removed",
+                Status = true,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        catch (Exception e)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Data = e.StackTrace,
+                Code = StatusCodes.Status500InternalServerError,
+                Message = e.Message,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+            });
+        }
+    }
+    
+    
+    
 }
