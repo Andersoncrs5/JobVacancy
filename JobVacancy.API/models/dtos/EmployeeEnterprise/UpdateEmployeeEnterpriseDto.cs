b38e554b6433143e2964ee5f05f7fb793a716589
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-09 11:50:08 -0300

    change column SalaryValue value of decimal to decimal?

--- JobVacancy.API/models/dtos/EmployeeEnterprise/UpdateEmployeeEnterpriseDto.cs
+++ JobVacancy.API/models/dtos/EmployeeEnterprise/UpdateEmployeeEnterpriseDto.cs
@@ -8,7 +8,7 @@ public class UpdateEmployeeEnterpriseDto
     public string? SalaryRange {get; set;}
     public string? TerminationReason {get; set;} 
     public string? Notes {get; set;}
-    public decimal SalaryValue {get; set;}
+    public decimal? SalaryValue {get; set;}
     public PaymentFrequencyEnum? PaymentFrequency {get; set;}
     public ContractLegalTypeEnum? ContractLegalType {get; set;}
     public EmploymentTypeEnum? ContractType {get; set;} 
