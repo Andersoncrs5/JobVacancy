89693cd4591db88ead45a9d33d2fde9032115772
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-10 20:37:04 -0300

    Added validation

--- JobVacancy.API/models/dtos/ReviewEnterprise/UpdateReviewEnterpriseDto.cs
+++ JobVacancy.API/models/dtos/ReviewEnterprise/UpdateReviewEnterpriseDto.cs
@@ -1,14 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobVacancy.API.models.dtos.ReviewEnterprise;
 
 public class UpdateReviewEnterpriseDto
 {
+    [MaxLength(299)]
     public string? Title { get; set; }
-    public string? Content { get; set; }
-    public int? RatingOverall { get; set; }
     
+    [MaxLength(800)]
+    public string? Content { get; set; }
+    [Range(0 , 5)]
+    public int? RatingOverall { get; set; }
+    [Range(0 , 5)]
     public int? RatingCulture { get; set; }
+    [Range(0 , 5)]
     public int? RatingCompensation { get; set; }
+    [Range(0 , 5)]
     public int? RatingManagement { get; set; }
+    [Range(0 , 5)]
     public int? RatingWorkLifeBalance { get; set; }
     
     public bool? IsAnonymous { get; set; }
