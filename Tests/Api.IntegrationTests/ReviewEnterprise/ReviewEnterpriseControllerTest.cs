55e57e8a6cdbebedc8ed430a49c6665186405efa
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-10 18:20:56 -0300

    Impled integration test to ReviewEnterpriseController

---
+++ Tests/Api.IntegrationTests/ReviewEnterprise/ReviewEnterpriseControllerTest.cs
@@ -0,0 +1,403 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using FluentAssertions;
+using JobVacancy.API.IntegrationTests.Utils;
+using JobVacancy.API.models.dtos.EmployeeEnterprise;
+using JobVacancy.API.models.dtos.EmployeeInvitation;
+using JobVacancy.API.models.dtos.Enterprise;
+using JobVacancy.API.models.dtos.Industry;
+using JobVacancy.API.models.dtos.Position;
+using JobVacancy.API.models.dtos.ReviewEnterprise;
+using JobVacancy.API.models.entities.Enums;
+using JobVacancy.API.Utils.Res;
+using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
+
+namespace JobVacancy.API.IntegrationTests.ReviewEnterprise;
+
+public class ReviewEnterpriseControllerTest: IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+    private readonly Helper _helper;
+    private readonly IConfiguration _configuration;
+    private readonly string _url = "/api/v1/ReviewEnterprise";
+    private readonly ITestOutputHelper _output;
+    
+    public ReviewEnterpriseControllerTest(CustomWebApplicationFactory factory, ITestOutputHelper output) 
+    {
+        _client = factory.CreateClient(); 
+        _helper = new Helper(_client); 
+        _configuration = factory.Configuration;
+        _output = output;
+    }
+
+    [Fact]
+    public async Task Create()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        EmployeeEnterpriseDto employeeDto = await _helper.CreateEmployeeEnterprise(invitationUpdated);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        CreateReviewEnterpriseDto reviewDto = new CreateReviewEnterpriseDto()
+        {
+            Title = string.Concat(Enumerable.Repeat("HelloWorld", 20)),
+            Content = string.Concat(Enumerable.Repeat("HelloWorld", 50)),
+            IsAnonymous = true,
+            RatingOverall = Random.Shared.Next(0, 5),
+            RatingCulture = Random.Shared.Next(0, 5),
+            RatingManagement = Random.Shared.Next(0, 5),
+            RatingCompensation = Random.Shared.Next(0, 5),
+            RatingWorkLifeBalance = Random.Shared.Next(0, 5),
+        };
+
+        HttpResponseMessage message = await _client.PostAsJsonAsync($"{_url}/{enterprise.Id}", reviewDto);
+        message.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        ResponseHttp<ReviewEnterpriseDto>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<ReviewEnterpriseDto>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.Created);
+        http.Status.Should().BeTrue();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().NotBeNull();
+        http.Data.Id.Should().NotBeNullOrWhiteSpace();
+        http.Data.Title.Should().Be(reviewDto.Title);
+        http.Data.Content.Should().Be(reviewDto.Content);
+        http.Data.IsAnonymous.Should().Be(reviewDto.IsAnonymous);
+        http.Data.RatingOverall.Should().Be(reviewDto.RatingOverall);
+        http.Data.RatingCulture.Should().Be(reviewDto.RatingCulture);
+        http.Data.RatingManagement.Should().Be(reviewDto.RatingManagement);
+        http.Data.RatingCompensation.Should().Be(reviewDto.RatingCompensation);
+        http.Data.RatingWorkLifeBalance.Should().Be(reviewDto.RatingWorkLifeBalance);
+    }
+    
+    [Fact]
+    public async Task CreateReturnAlreadyExists()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        EmployeeEnterpriseDto employeeDto = await _helper.CreateEmployeeEnterprise(invitationUpdated);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        ReviewEnterpriseDto reviewCreated = await _helper.CreateReviewEnterprise(invitation);
+
+        CreateReviewEnterpriseDto reviewDto = new CreateReviewEnterpriseDto()
+        {
+            Title = string.Concat(Enumerable.Repeat("HelloWorld", 20)),
+            Content = string.Concat(Enumerable.Repeat("HelloWorld", 50)),
+            IsAnonymous = true,
+            RatingOverall = Random.Shared.Next(0, 5),
+            RatingCulture = Random.Shared.Next(0, 5),
+            RatingManagement = Random.Shared.Next(0, 5),
+            RatingCompensation = Random.Shared.Next(0, 5),
+            RatingWorkLifeBalance = Random.Shared.Next(0, 5),
+        };
+
+        HttpResponseMessage message = await _client.PostAsJsonAsync($"{_url}/{reviewCreated.EnterpriseId}", reviewDto);
+        message.StatusCode.Should().Be(HttpStatusCode.Conflict);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.Conflict);
+        http.Status.Should().BeFalse();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task CreateNotFoundEnterprise()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        EmployeeEnterpriseDto employeeDto = await _helper.CreateEmployeeEnterprise(invitationUpdated);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        CreateReviewEnterpriseDto reviewDto = new CreateReviewEnterpriseDto()
+        {
+            Title = string.Concat(Enumerable.Repeat("HelloWorld", 20)),
+            Content = string.Concat(Enumerable.Repeat("HelloWorld", 50)),
+            IsAnonymous = true,
+            RatingOverall = Random.Shared.Next(0, 5),
+            RatingCulture = Random.Shared.Next(0, 5),
+            RatingManagement = Random.Shared.Next(0, 5),
+            RatingCompensation = Random.Shared.Next(0, 5),
+            RatingWorkLifeBalance = Random.Shared.Next(0, 5),
+        };
+
+        HttpResponseMessage message = await _client.PostAsJsonAsync($"{_url}/{Guid.NewGuid()}", reviewDto);
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.NotFound);
+        http.Status.Should().BeFalse();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task CreateNotFoundEmployeeEnterprise()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        CreateReviewEnterpriseDto reviewDto = new CreateReviewEnterpriseDto()
+        {
+            Title = string.Concat(Enumerable.Repeat("HelloWorld", 20)),
+            Content = string.Concat(Enumerable.Repeat("HelloWorld", 50)),
+            IsAnonymous = true,
+            RatingOverall = Random.Shared.Next(0, 5),
+            RatingCulture = Random.Shared.Next(0, 5),
+            RatingManagement = Random.Shared.Next(0, 5),
+            RatingCompensation = Random.Shared.Next(0, 5),
+            RatingWorkLifeBalance = Random.Shared.Next(0, 5),
+        };
+
+        HttpResponseMessage message = await _client.PostAsJsonAsync($"{_url}/{enterprise.Id}", reviewDto);
+        message.StatusCode.Should().Be(HttpStatusCode.Conflict);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.Conflict);
+        http.Status.Should().BeFalse();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task Get()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        EmployeeEnterpriseDto employeeDto = await _helper.CreateEmployeeEnterprise(invitationUpdated);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        ReviewEnterpriseDto reviewDto = await _helper.CreateReviewEnterprise(invitation);
+
+        HttpResponseMessage message = await _client.GetAsync($"{_url}/{reviewDto.Id}");
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ResponseHttp<ReviewEnterpriseDto>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<ReviewEnterpriseDto>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.OK);
+        http.Status.Should().BeTrue();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().NotBeNull();
+        http.Data.Id.Should().Be(reviewDto.Id);
+    }
+    
+    [Fact]
+    public async Task GetNotFound()
+    {
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}/{Guid.NewGuid()}");
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.NotFound);
+        http.Status.Should().BeFalse();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task Del()
+    {
+        ResponseTokens master = await _helper.LoginMaster(_configuration);
+        IndustryDto industryDto = await _helper.CreateIndustry(master);
+        PositionDto positionDto = await _helper.CreatePositionAsync();
+
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+        UserResultTest user = await _helper.CreateAndGetUser();
+        EnterpriseDto enterprise = await _helper.CreateEnterprise(user, industryDto);
+
+        ResponseTokens loginUserWithNewRole = await _helper.LoginUser(user!.User!.Email!, user.CreateUser!.PasswordHash);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token!);
+
+        EmployeeInvitationDto invitation = await _helper.CreateEmployeeInvitation(userGuest, positionDto);
+        
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        EmployeeInvitationDto invitationUpdated = await _helper.UpdateInvitationByUser(StatusEnum.Accepted, invitation);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", loginUserWithNewRole.Token);
+        
+        EmployeeEnterpriseDto employeeDto = await _helper.CreateEmployeeEnterprise(invitationUpdated);
+
+        _client.DefaultRequestHeaders.Authorization = 
+            new AuthenticationHeaderValue("Bearer", userGuest.Tokens!.Token);
+
+        ReviewEnterpriseDto reviewDto = await _helper.CreateReviewEnterprise(invitation);
+
+        HttpResponseMessage message = await _client.DeleteAsync($"{_url}/{reviewDto.Id}");
+        message.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.NoContent);
+        http.Status.Should().BeTrue();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task DelNotFound()
+    {
+        
+        UserResultTest userGuest = await _helper.CreateAndGetUser();
+
+        HttpResponseMessage message = await _client.DeleteAsync($"{_url}/{Guid.NewGuid()}");
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        http.Code.Should().Be((int)HttpStatusCode.NotFound);
+        http.Status.Should().BeFalse();
+        http.Version.Should().Be(1);
+        
+        http.Data.Should().BeNull();
+    }
+    
+    
+    
+}
