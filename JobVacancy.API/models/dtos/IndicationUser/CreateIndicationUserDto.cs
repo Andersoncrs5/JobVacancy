05813878458385d9509c3377b4a2e0042db21ccc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-11 20:14:53 -0300

    Added validation

--- JobVacancy.API/models/dtos/IndicationUser/CreateIndicationUserDto.cs
+++ JobVacancy.API/models/dtos/IndicationUser/CreateIndicationUserDto.cs
@@ -1,16 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 using JobVacancy.API.models.entities.Enums;
+using JobVacancy.API.Utils.Annotations.Validations;
 
 namespace JobVacancy.API.models.dtos.IndicationUser;
 
 public class CreateIndicationUserDto
 {
     [Required]
+    [ExistsUserById]
     public string EndorsedId { get; set; }  = string.Empty;
     [Required]
+    [MaxLength(500)]
     public string Content { get; set; } = string.Empty;
-    [Required]
-    public IndicationStatusEnum Status { get; set; }
     [Range(0, 10)]
     public int? SkillRating { get; set; }
 }
