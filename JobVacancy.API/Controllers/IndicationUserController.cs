6e5aa6ae156df072df4f92d46c6e5e9cf4e61388
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-11 21:06:33 -0300

    New endpoint GetAll, Patch and PatchByEndorsed

--- JobVacancy.API/Controllers/IndicationUserController.cs
+++ JobVacancy.API/Controllers/IndicationUserController.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using JobVacancy.API.models.dtos.IndicationUser;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.IndicationUser;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -149,7 +151,132 @@ public class IndicationUserController(
         });
         
     }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Page<IndicationUserDto>))]
+    public async Task<IActionResult> GetAll([FromQuery] IndicationUserFilterParam filter)
+    {
+        IQueryable<IndicationUserEntity> iQueryable = indicationUserService.Query();
+        IQueryable<IndicationUserEntity> appliedFilter = IndicationUserFilterQuery.ApplyFilter(iQueryable, filter);
+        PaginatedList<IndicationUserEntity> paginatedList = await PaginatedList<IndicationUserEntity>.CreateAsync(
+            source: appliedFilter,
+            pageSize: filter.PageSize,
+            pageIndex: filter.PageNumber
+        );
+        
+        Page<IndicationUserDto> dtos = mapper.Map<Page<IndicationUserDto>>(paginatedList);
+        
+        return Ok(dtos);
+    }
     
+    [HttpPatch("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<IndicationUserDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> Patch(string id, [FromBody] UpdateIndicationUserEndorserDto endorserDto)
+    {
+        string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (userId == null) return Unauthorized();
+        
+        IndicationUserEntity? indication = await indicationUserService.GetById(id);
+        if (indication == null)
+        {
+            return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Data = null,
+                Message = "Indication User not found",
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false
+            });
+        }
+        
+        if (indication.EndorserId != userId)
+        {
+            return StatusCode(StatusCodes.Status409Conflict, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status409Conflict,
+                Data = null,
+                Message = "You do not have permission to modify this endorsement.",
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false
+            });
+        }
+        
+        IndicationUserEntity update = await indicationUserService.Update(endorserDto, indication);
+
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<IndicationUserDto>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = mapper.Map<IndicationUserDto>(update),
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1,
+            Status = true,
+            Message = "Indication updated"
+        });
+        
+    }
+
     
+    [HttpPatch("{id:required}/By/Endorsed")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<IndicationUserDto>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> PatchByEndorsed(string id, [FromBody] UpdateIndicationUserEndorsedDto endorserDto)
+    {
+        string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (userId == null) return Unauthorized();
+        
+        IndicationUserEntity? indication = await indicationUserService.GetById(id);
+        if (indication == null)
+        {
+            return StatusCode(StatusCodes.Status404NotFound, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Data = null,
+                Message = "Indication User not found",
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false
+            });
+        }
+        
+        if (indication.EndorsedId != userId)
+        {
+            return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status403Forbidden,
+                Data = null,
+                Message = "You do not have permission to modify this endorsement.",
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false
+            });
+        }
+        
+        IndicationUserEntity update = await indicationUserService.UpdateByEndorsed(endorserDto, indication);
+
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<IndicationUserDto>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = mapper.Map<IndicationUserDto>(update),
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1,
+            Status = true,
+            Message = "Indication updated"
+        });
+        
+    }
+
     
 }
