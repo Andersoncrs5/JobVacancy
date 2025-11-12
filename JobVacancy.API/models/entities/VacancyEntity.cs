562f62f2aaee06e9f003047a2ab06a4d93b4a940
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-12 15:26:56 -0300

    New table VacancyEntity

---
+++ JobVacancy.API/models/entities/VacancyEntity.cs
@@ -0,0 +1,35 @@
+using JobVacancy.API.models.entities.Enums;
+
+namespace JobVacancy.API.models.entities;
+
+public class VacancyEntity: BaseEntity
+{
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+    public string? Requirements { get; set; }
+    public string? Responsibilities { get; set; }
+    public string? Benefits { get; set; }
+
+    public EmploymentTypeEnum EmploymentType { get; set; }
+    public ExperienceLevelEnum? ExperienceLevel { get; set; }
+    public EducationLevelEnum? EducationLevel { get; set; }
+    public WorkplaceTypeEnum? WorkplaceType { get; set; }
+    public CurrencyEnum? Currency { get; set; }
+    public VacancyStatusEnum Status { get; set; }
+
+    public int? Seniority { get; set; }
+    public int Opening { get; set; }
+
+    public decimal? SalaryMin { get; set; }
+    public decimal? SalaryMax { get; set; }
+
+    public string EnterpriseId { get; set; }
+    public EnterpriseEntity? Enterprise { get; set; }
+
+    public string AreaId { get; set; }
+    public AreaEntity? Area { get; set; }
+
+    public DateTime? ApplicationDeadLine { get; set; }
+    public DateTime? LastApplication { get; set; } 
+}
