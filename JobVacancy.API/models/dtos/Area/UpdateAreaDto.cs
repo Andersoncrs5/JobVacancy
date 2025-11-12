002a1fd854817db139b82ac722ed425c8f5c6da8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-12 14:41:35 -0300

    Impled UpdateAreaDto

---
+++ JobVacancy.API/models/dtos/Area/UpdateAreaDto.cs
@@ -0,0 +1,8 @@
+namespace JobVacancy.API.models.dtos.Area;
+
+public class UpdateAreaDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool? IsActive { get; set; }
+}
