a2a35b96ec0f0cab56a2e4c587e4c411a689b5c5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-12 15:19:18 -0300

    New table EducationLevelEnum

---
+++ JobVacancy.API/models/entities/Enums/EducationLevelEnum.cs
@@ -0,0 +1,11 @@
+namespace JobVacancy.API.models.entities.Enums;
+
+public enum EducationLevelEnum
+{
+    None = 0, 
+    HighSchool = 1, 
+    Technical = 2, 
+    Bachelor = 3, 
+    Master = 4, 
+    Doctorate = 5, 
+}
