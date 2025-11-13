f85bac218c55c349c896b76b592584042734708e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-12 21:14:36 -0300

    Impled validation

--- JobVacancy.API/models/dtos/Vacancy/CreateVacancyDto.cs
+++ JobVacancy.API/models/dtos/Vacancy/CreateVacancyDto.cs
@@ -1,17 +1,23 @@
 using System.ComponentModel.DataAnnotations;
 using JobVacancy.API.models.entities.Enums;
+using JobVacancy.API.Utils.Annotations.Validations.Area;
 
 namespace JobVacancy.API.models.dtos.Vacancy;
 
 public class CreateVacancyDto
 {
     [Required]
+    [MaxLength(300)]
     public string Title { get; set; }
     
     [Required]
+    [MaxLength(3000)]
     public string Description { get; set; }
+    [MaxLength(1500)]
     public string? Requirements { get; set; }
+    [MaxLength(1500)]
     public string? Responsibilities { get; set; }
+    [MaxLength(1500)]
     public string? Benefits { get; set; }
     
     [Required]
@@ -21,12 +27,18 @@ public class CreateVacancyDto
     public WorkplaceTypeEnum? WorkplaceType { get; set; }
     public CurrencyEnum? Currency { get; set; }
     
+    [Range(0, 10)]
     public int? Seniority { get; set; }
+    [Range(0, 20)]
     public int Opening { get; set; }
 
     public decimal? SalaryMin { get; set; }
     public decimal? SalaryMax { get; set; }
     
     [Required]
+    [ExistsAreaById]
     public string AreaId { get; set; }
+    
+    [DataType(DataType.DateTime)]
+    public DateTime? ApplicationDeadLine { get; set; }
 }
