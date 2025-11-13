307c7ab0825b77f755789cc1c5475c57413e9dda
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-12 21:55:13 -0300

    New UpdateVacancyDto

---
+++ JobVacancy.API/models/dtos/VacancySkill/UpdateVacancyDto.cs
@@ -0,0 +1,13 @@
+using JobVacancy.API.models.entities.Enums;
+
+namespace JobVacancy.API.models.dtos.VacancySkill;
+
+public class UpdateVacancyDto
+{
+    public string? SkillId { get; set; }
+    public SkillProficiencyLevelEnum? RequiredLevel { get; set; }
+    public bool? IsMandatory { get; set; }
+    public int? Weight { get; set; }
+    public int? YearsOfExperienceRequired { get; set; }
+    public string? Notes { get; set; }
+}
