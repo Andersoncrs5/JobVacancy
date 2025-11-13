b5b8867dc57fb7ab31f0bb4f71af438836b49167
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-13 20:30:35 -0300

    added validations

--- JobVacancy.API/models/dtos/ApplicationVacancy/CreateApplicationVacancyDto.cs
+++ JobVacancy.API/models/dtos/ApplicationVacancy/CreateApplicationVacancyDto.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using JobVacancy.API.Utils.Annotations.Validations.Vacancy;
+
 namespace JobVacancy.API.models.dtos.ApplicationVacancy;
 
 public class CreateApplicationVacancyDto
 {
+    [Required]
+    [ExistsVacancyById]
     public required string VacancyId { get; set; }
     public string? CoverLetter { get; set; }
 }
