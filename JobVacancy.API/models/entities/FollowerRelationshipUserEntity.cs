18d7ed8479aef19b0f0880f79586a8e454558dbe
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-14 15:30:24 -0300

    New FollowerRelationshipUserEntity

---
+++ JobVacancy.API/models/entities/FollowerRelationshipUserEntity.cs
@@ -0,0 +1,16 @@
+using JobVacancy.API.models.dtos;
+
+namespace JobVacancy.API.models.entities;
+
+public class FollowerRelationshipUserEntity: BaseEntity
+{
+    public required string FollowerId { get; set; }
+    public UserEntity? Follower { get; set; }
+    
+    public required string FollowedId { get; set; }
+    public UserEntity? Followed { get; set; }
+    
+    public bool WishReceiveNotifyByNewPost { get; set; } = true; 
+    public bool WishReceiveNotifyByNewComment { get; set; } = true;
+    public bool WishReceiveNotifyByNewInteraction { get; set; } = false;
+}
