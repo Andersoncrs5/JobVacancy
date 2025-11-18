645c46b46f2a0685feecc20f04b551eb21ba0acf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-18 20:38:22 -0300

    More tests

--- Tests/Api.IntegrationTests/ReviewUser/ReviewUserControllerTest.cs
+++ Tests/Api.IntegrationTests/ReviewUser/ReviewUserControllerTest.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Json;
 using FluentAssertions;
 using JobVacancy.API.IntegrationTests.Utils;
 using JobVacancy.API.models.dtos.ReviewUser;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.Extensions.Configuration;
 using Xunit.Abstractions;
@@ -139,7 +140,7 @@ public class ReviewUserControllerTest: IClassFixture<CustomWebApplicationFactory
     }
 
     [Fact]
-    public async Task GetNotFound()
+    public async Task GetNotFound() 
     {
         UserResultTest actor = await _helper.CreateAndGetUser();
 
@@ -148,7 +149,7 @@ public class ReviewUserControllerTest: IClassFixture<CustomWebApplicationFactory
         HttpResponseMessage message = await _client.GetAsync($"{_url}/{Guid.NewGuid()}");
         message.StatusCode.Should().Be(HttpStatusCode.NotFound);
 
-        ResponseHttp<ReviewUserDto>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<ReviewUserDto>>();
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
         http.Should().NotBeNull();
         http.Code.Should().Be((int)HttpStatusCode.NotFound);
         http.Status.Should().BeFalse();
@@ -157,4 +158,279 @@ public class ReviewUserControllerTest: IClassFixture<CustomWebApplicationFactory
         http.Data.Should().BeNull();
     }
 
+    [Fact]
+    public async Task Del() 
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+
+        HttpResponseMessage message = await _client.DeleteAsync($"{_url}/{reviewDto.Id}");
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.OK);
+        http.Status.Should().BeTrue();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task DelNotFound() 
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+
+        HttpResponseMessage message = await _client.DeleteAsync($"{_url}/{Guid.NewGuid()}");
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.NotFound);
+        http.Status.Should().BeFalse();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task DelReturnForb() 
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", target.Tokens!.Token!);
+        
+        HttpResponseMessage message = await _client.DeleteAsync($"{_url}/{reviewDto.Id}");
+        message.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.Forbidden);
+        http.Status.Should().BeFalse();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+
+        http.Data.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task Patch()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+
+        UpdateReviewUserDto dto = new UpdateReviewUserDto()
+        {
+            Content = string.Concat(Enumerable.Repeat("ABC", 10)),
+            IsAnonymous = !reviewDto.IsAnonymous,
+            RatingCompensation = 1,
+            RatingCulture = 1,
+            RatingManagement = 1,
+            RatingOverall = 1,
+            RatingWorkLifeBalance = 1,
+            Recommendation = !reviewDto.Recommendation,
+            Title = string.Concat(Enumerable.Repeat("ABC", 10)),
+        };
+        
+        HttpResponseMessage message = await _client.PatchAsJsonAsync($"{_url}/{reviewDto.Id}", dto);
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ResponseHttp<ReviewUserDto>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<ReviewUserDto>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.OK);
+        http.Status.Should().BeTrue();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        
+        http.Data.Should().NotBeNull();
+        http.Data.Id.Should().Be(reviewDto.Id);
+        http.Data.Title.Should().Be(dto.Title);
+        http.Data.IsAnonymous.Should().Be(dto.IsAnonymous.Value);
+        http.Data.RatingCompensation.Should().Be(dto.RatingCompensation);
+        http.Data.RatingCulture.Should().Be(dto.RatingCulture);
+        http.Data.RatingManagement.Should().Be(dto.RatingManagement);
+        http.Data.RatingOverall.Should().Be(dto.RatingOverall);
+        http.Data.RatingWorkLifeBalance.Should().Be(dto.RatingWorkLifeBalance);
+        http.Data.Recommendation.Should().Be(dto.Recommendation);
+        http.Data.TargetUserId.Should().Be(reviewDto.TargetUserId);
+        http.Data.Content.Should().Be(dto.Content);
+    }
+
+    [Fact]
+    public async Task PatchNotFound()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+
+        UpdateReviewUserDto dto = new UpdateReviewUserDto()
+        {
+            Content = string.Concat(Enumerable.Repeat("ABC", 10)),
+            IsAnonymous = false,
+            RatingCompensation = 1,
+            RatingCulture = 1,
+            RatingManagement = 1,
+            RatingOverall = 1,
+            RatingWorkLifeBalance = 1,
+            Recommendation = false,
+            Title = string.Concat(Enumerable.Repeat("ABC", 10)),
+        };
+        
+        HttpResponseMessage message = await _client.PatchAsJsonAsync($"{_url}/{Guid.NewGuid()}", dto);
+        message.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.NotFound);
+        http.Status.Should().BeFalse();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        
+        http.Data.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task PatchReturnConflict()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+        
+        UpdateReviewUserDto dto = new UpdateReviewUserDto()
+        {
+            Content = string.Concat(Enumerable.Repeat("ABC", 10)),
+            IsAnonymous = false,
+            RatingCompensation = 1,
+            RatingCulture = 1,
+            RatingManagement = 1,
+            RatingOverall = 1,
+            RatingWorkLifeBalance = 1,
+            Recommendation = false,
+            Title = string.Concat(Enumerable.Repeat("ABC", 10)),
+        };
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", target.Tokens!.Token!);
+        
+        HttpResponseMessage message = await _client.PatchAsJsonAsync($"{_url}/{reviewDto.Id}", dto);
+        message.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+
+        ResponseHttp<object>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<object>>();
+        http.Should().NotBeNull();
+        http.Code.Should().Be((int)HttpStatusCode.Forbidden);
+        http.Status.Should().BeFalse();
+        http.Message.Should().NotBeNullOrWhiteSpace();
+        
+        http.Data.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task GetAllSimple()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}");
+        _output.WriteLine(await message.Content.ReadAsStringAsync());
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        Page<object>? http = await message.Content.ReadFromJsonAsync<Page<object>>();
+        http.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task GetAllJustLoadTarget()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}?LoadTarget={true}");
+        _output.WriteLine(await message.Content.ReadAsStringAsync());
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        Page<ReviewUserDto>? http = await message.Content.ReadFromJsonAsync<Page<ReviewUserDto>>();
+        http.Should().NotBeNull();
+        http.Data.Should().NotBeNull();
+        
+        http.Data.First().Should().NotBeNull();
+        http.Data.First().TargetUser.Should().NotBeNull();
+        http.Data.First().Actor.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task GetAllJustLoadActor()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}?LoadActor={true}");
+        _output.WriteLine(await message.Content.ReadAsStringAsync());
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        Page<ReviewUserDto>? http = await message.Content.ReadFromJsonAsync<Page<ReviewUserDto>>();
+        http.Should().NotBeNull();
+        http.Data.Should().NotBeNull();
+        
+        http.Data.First().Should().NotBeNull();
+        http.Data.First().Actor.Should().NotBeNull();
+        http.Data.First().TargetUser.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task ExistsTrue()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}/{target.User!.Id}/Exists");
+        _output.WriteLine(await message.Content.ReadAsStringAsync());
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ResponseHttp<bool>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<bool>>();
+        http.Should().NotBeNull();
+
+        http.Data.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task ExistsFalse()
+    {
+        UserResultTest actor = await _helper.CreateAndGetUser();
+        UserResultTest target = await _helper.CreateAndGetUser();
+        
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", actor.Tokens!.Token!);
+        ReviewUserDto reviewDto = await _helper.CreateReviewToUser(target, actor);
+        
+        HttpResponseMessage message = await _client.GetAsync($"{_url}/{Guid.NewGuid()}/Exists");
+        _output.WriteLine(await message.Content.ReadAsStringAsync());
+        message.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        ResponseHttp<bool>? http = await message.Content.ReadFromJsonAsync<ResponseHttp<bool>>();
+        http.Should().NotBeNull();
+
+        http.Data.Should().BeFalse();
+    }
+    
+    
 }
