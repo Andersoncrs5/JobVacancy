c34adf810587af72d6ef29fc61c6514842ad82c3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-18 14:07:40 -0300

    new migrations

---
+++ JobVacancy.API/Migrations/20251116235327_RemovedDubleKeyOfTableEnterpriseFollowsUser.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobVacancy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedDubleKeyOfTableEnterpriseFollowsUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EnterpriseFollowsUser",
+                table: "EnterpriseFollowsUser");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "EnterpriseFollowsUser",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EnterpriseFollowsUser",
+                table: "EnterpriseFollowsUser",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnterpriseFollowsUser_EnterpriseId_UserId",
+                table: "EnterpriseFollowsUser",
+                columns: new[] { "EnterpriseId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EnterpriseFollowsUser",
+                table: "EnterpriseFollowsUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_EnterpriseFollowsUser_EnterpriseId_UserId",
+                table: "EnterpriseFollowsUser");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "EnterpriseFollowsUser",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EnterpriseFollowsUser",
+                table: "EnterpriseFollowsUser",
+                columns: new[] { "EnterpriseId", "UserId" });
+        }
+    }
+}
