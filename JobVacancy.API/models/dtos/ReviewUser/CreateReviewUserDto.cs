81565ea4d5026ca8383aaa2c57141a374aa1a718
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-18 15:09:49 -0300

    New CreateReviewUserDto

---
+++ JobVacancy.API/models/dtos/ReviewUser/CreateReviewUserDto.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using JobVacancy.API.Utils.Annotations.Validations;
+
+namespace JobVacancy.API.models.dtos.ReviewUser;
+
+public class CreateReviewUserDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    
+    [Required]
+    [Range(0 , 5)]
+    public int RatingOverall { get; set; }
+    
+    [Range(0 , 5)]
+    public int? RatingCulture { get; set; }
+    [Range(0 , 5)]   
+    public int? RatingCompensation { get; set; }
+    [Range(0 , 5)]   
+    public int? RatingManagement { get; set; }
+    [Range(0 , 5)]   
+    public int? RatingWorkLifeBalance { get; set; }
+
+    public bool? Recommendation { get; set; }
+    public bool IsAnonymous { get; set; }
+    
+    [ExistsUserById]
+    public required string TargetUserId { get; set; }
+}
