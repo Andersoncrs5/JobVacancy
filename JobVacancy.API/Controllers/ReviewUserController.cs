d906c52f39b541f7d1092dbfadeac9566bba47a7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-18 20:38:14 -0300

    New endpoints Delete, Patch, GetAll and Exists

--- JobVacancy.API/Controllers/ReviewUserController.cs
+++ JobVacancy.API/Controllers/ReviewUserController.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using JobVacancy.API.models.dtos.ReviewUser;
 using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.ReviewUser;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -21,7 +23,7 @@ public class ReviewUserController(
     ): Controller
 {
 
-    [HttpGet("{id:guid}")]
+    [HttpGet("{id:required}")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<ReviewUserDto>))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
     public async Task<IActionResult> Get(string id)
@@ -46,6 +48,130 @@ public class ReviewUserController(
             Code = StatusCodes.Status200OK,
             Data = mapper.Map<ReviewUserDto>(review),
             Message = "Review found",
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1
+        });
+    }
+    
+    [HttpDelete("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (string.IsNullOrEmpty(userId)) return Unauthorized();
+        
+        ReviewUserEntity? review = await reviewUserService.GetById(id);
+        if (review == null) 
+        {
+            return StatusCode( StatusCodes.Status404NotFound, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Message = "Review not found",
+                Data = null,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        
+        if (review.ActorId != userId)
+        {
+            return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status403Forbidden,
+                Message = "You do not have permission to delete this review",
+                Data = null,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        
+        await reviewUserService.Delete(review);
+        
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = null,
+            Message = "Review found",
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1
+        });
+    }
+    
+    [HttpPatch("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> Patch(string id, [FromBody] UpdateReviewUserDto dto )
+    {
+        string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (string.IsNullOrEmpty(userId)) return Unauthorized();
+        
+        ReviewUserEntity? review = await reviewUserService.GetById(id);
+        if (review == null) 
+        {
+            return StatusCode( StatusCodes.Status404NotFound, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Message = "Review not found",
+                Data = null,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        
+        if (review.ActorId != userId) 
+        {
+            return StatusCode(StatusCodes.Status403Forbidden, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status403Forbidden,
+                Message = "You do not have permission to update this review",
+                Data = null,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+
+        DateTime limitDate = review.CreatedAt.AddMonths(1);
+
+        if (DateTime.UtcNow > limitDate)
+        {
+            return StatusCode( StatusCodes.Status410Gone, new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status410Gone,
+                Message = "Review cannot be updated",
+                Data = null,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1
+            });
+        }
+        
+        ReviewUserEntity update = await reviewUserService.Update(dto, review);
+
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = mapper.Map<ReviewUserDto>(update),
+            Message = "Review updated",
             Status = true,
             Timestamp = DateTimeOffset.UtcNow,
             TraceId = HttpContext.TraceIdentifier,
@@ -92,5 +218,44 @@ public class ReviewUserController(
             Version = 1
         });
     }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Page<ReviewUserDto>))]
+    public async Task<IActionResult> GetAll([FromQuery] ReviewUserFilterParams filter)
+    {
+        IQueryable<ReviewUserEntity> iQueryable = reviewUserService.Query();
+        IQueryable<ReviewUserEntity> appliedFilter = ReviewUserFilterQuery.ApplyFilter(iQueryable, filter);
+        PaginatedList<ReviewUserEntity> paginatedList = await PaginatedList<ReviewUserEntity>.CreateAsync(
+            source: appliedFilter,
+            pageSize: filter.PageSize,
+            pageIndex: filter.PageNumber
+        );
+        
+        Page<ReviewUserDto> dtos = mapper.Map<Page<ReviewUserDto>>(paginatedList);
+        
+        return Ok(dtos);
+    }
+
+    [HttpGet("{userId}/Exists")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<bool>))]
+    public async Task<IActionResult> Exists(string userId)
+    {
+        string? actorId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (string.IsNullOrEmpty(actorId)) return Unauthorized();
+
+        bool exists = await reviewUserService.ExistsByActorIdAndTargetId(actorId, userId);
+    
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<bool>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = exists,
+            Message = "",
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1
+        });
+    }
+    
     
 }
