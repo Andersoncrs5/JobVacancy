6ce0da19111ff6ec9137db5a210ecf49cf3e955d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-19 14:19:00 -0300

    Impled UserEvaluationRepository interface and provider

---
+++ JobVacancy.API/Repositories/Interfaces/IUserEvaluationRepository.cs
@@ -0,0 +1,8 @@
+using JobVacancy.API.models.entities;
+
+namespace JobVacancy.API.Repositories.Interfaces;
+
+public interface IUserEvaluationRepository: IGenericRepository<UserEvaluationEntity>
+{
+    Task<bool> ExistsByEnterpriseIdAndTargetUserId(string enterpriseId, string targetUserId);
+}
---
+++ JobVacancy.API/Repositories/Provider/UserEvaluationRepository.cs
@@ -0,0 +1,13 @@
+using JobVacancy.API.Context;
+using JobVacancy.API.models.entities;
+using JobVacancy.API.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobVacancy.API.Repositories.Provider;
+
+public class UserEvaluationRepository(AppDbContext ctx) : GenericRepository<UserEvaluationEntity>(ctx),
+    IUserEvaluationRepository
+{
+    public async Task<bool> ExistsByEnterpriseIdAndTargetUserId(string enterpriseId, string targetUserId)
+        => await ctx.UserEvaluationEntities.AnyAsync(x => x.EnterpriseId == enterpriseId && x.TargetUserId == targetUserId);
+}
