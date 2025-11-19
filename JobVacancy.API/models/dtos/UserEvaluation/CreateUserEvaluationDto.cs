d812d6c07d745cce83270e746b62875efcca3f5f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-19 14:57:20 -0300

    New CreateUserEvaluationDto

---
+++ JobVacancy.API/models/dtos/UserEvaluation/CreateUserEvaluationDto.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using JobVacancy.API.models.entities.Enums;
+using JobVacancy.API.Utils.Annotations.Validations;
+
+namespace JobVacancy.API.models.dtos.UserEvaluation;
+
+public class CreateUserEvaluationDto
+{
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public string Content { get; set; } = string.Empty;
+    
+    [Required]
+    [Range(0 , 5)]
+    public int RatingOverall { get; set; }
+    
+    [Range(0 , 5)] public int? RatingCulture { get; set; }
+    [Range(0 , 5)] public int? RatingCompensation { get; set; }
+    [Range(0 , 5)] public int? RatingManagement { get; set; }
+    [Range(0 , 5)] public int? RatingWorkLifeBalance { get; set; }
+    
+    public bool IsAnonymous { get; set; }
+    
+    [Range(0 , 5)] public int? RatingProfessionalism { get; set; } 
+    [Range(0 , 5)] public int? RatingSkillMatch { get; set; } 
+    [Range(0 , 5)] public int? RatingTeamwork { get; set; }
+    [Range(0 , 5)] public int? RecommendationTone { get; set; } 
+    
+    public EmploymentTypeEnum EmploymentStatus { get; set; } 
+    
+    [ExistsPosition]
+    public required string EnterpriseId { get; set; }
+    
+    [ExistsPosition] 
+    public required string TargetUserId { get; set; }
+    
+    public string? ReviewerUserId { get; set; }
+    
+    [ExistsPosition]
+    public string PositionId {get; set;} = string.Empty;
+}
