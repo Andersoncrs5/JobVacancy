75b2947db9dcffdd10a36e6769c1235e21c71443
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 10:42:47 -0300

    Added More params

--- JobVacancy.API/Utils/Filters/UserEvaluation/UserEvaluationFilterParams.cs
+++ JobVacancy.API/Utils/Filters/UserEvaluation/UserEvaluationFilterParams.cs
@@ -2,20 +2,37 @@ using JobVacancy.API.models.entities.Enums;
 
 namespace JobVacancy.API.Utils.Filters.UserEvaluation;
 
-public class UserEvaluationFilterParams
+public class UserEvaluationFilterParams: FilterBaseParams
 {
     public string? Title { get; set; } 
     public string? Content { get; set; } 
-    public int? RatingOverall { get; set; }
     
-    public int? RatingCulture { get; set; }
-    public int? RatingCompensation { get; set; }
-    public int? RatingManagement { get; set; }
-    public int? RatingWorkLifeBalance { get; set; }
-    public int? RatingProfessionalism { get; set; } 
-    public int? RatingSkillMatch { get; set; } 
-    public int? RatingTeamwork { get; set; }
-    public int? RecommendationTone { get; set; } 
+    public int? RatingOverallMin { get; set; }
+    public int? RatingOverallMax { get; set; }
+    
+    public int? RatingCultureMin { get; set; }
+    public int? RatingCultureMax { get; set; }
+    
+    public int? RatingCompensationMin { get; set; }
+    public int? RatingCompensationMax { get; set; }
+    
+    public int? RatingManagementMin { get; set; }
+    public int? RatingManagementMax { get; set; }
+    
+    public int? RatingWorkLifeBalanceMin { get; set; }
+    public int? RatingWorkLifeBalanceMax { get; set; }
+    
+    public int? RatingProfessionalismMin { get; set; } 
+    public int? RatingProfessionalismMax { get; set; } 
+    
+    public int? RatingSkillMatchMin { get; set; } 
+    public int? RatingSkillMatchMax { get; set; } 
+    
+    public int? RatingTeamworkMin { get; set; }
+    public int? RatingTeamworkMax { get; set; }
+    
+    public int? RecommendationToneMin { get; set; } 
+    public int? RecommendationToneMax { get; set; } 
     
     public bool? IsAnonymous { get; set; }
     
