dde71f10e2d13b5e9cc14dd49383446bfd0c2c1e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 20:37:02 -0300

    New KafkaConfig

---
+++ JobVacancy.API/Configs/kafka/Classes/KafkaConfig.cs
@@ -0,0 +1,9 @@
+namespace JobVacancy.API.Configs.kafka.Classes;
+
+public class KafkaConfig
+{
+    public string? BootstrapServers { get; set; }
+    public string? GroupId { get; set; }
+
+    public TopicNames? Topics { get; set; }
+}
