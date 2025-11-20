d5629587d9757201707cc90fb33e13b30d9d4983
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 18:29:08 -0300

    Removed param reactionType of methods ExistsByUserIdAndContentIdAndReaction and GetByUserIdAndContentIdAndReaction

--- JobVacancy.API/Repositories/Interfaces/IUserContentReactionRepository.cs
+++ JobVacancy.API/Repositories/Interfaces/IUserContentReactionRepository.cs
@@ -6,14 +6,12 @@ namespace JobVacancy.API.Repositories.Interfaces;
 public interface IUserContentReactionRepository: IGenericRepository<UserContentReactionEntity>
 {
     Task<bool> ExistsByUserIdAndContentIdAndReaction(
-        string userId, string contentId,
-        ReactionTypeEnum reactionType, ReactionTargetEnum targetType
+        string userId, string contentId, ReactionTargetEnum targetType
     );
 
     Task<UserContentReactionEntity?> GetByUserIdAndContentIdAndReaction(
         string userId,
-        string contentId,
-        ReactionTypeEnum reactionType,
+        string contentId, 
         ReactionTargetEnum targetType
     );
 
