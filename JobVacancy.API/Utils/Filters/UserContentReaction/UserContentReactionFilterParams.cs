0f933f082613c11d29f60aac587d9ac355660c42
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 18:30:17 -0300

    New parmas

--- JobVacancy.API/Utils/Filters/UserContentReaction/UserContentReactionFilterParams.cs
+++ JobVacancy.API/Utils/Filters/UserContentReaction/UserContentReactionFilterParams.cs
@@ -4,5 +4,9 @@ namespace JobVacancy.API.Utils.Filters.UserContentReaction;
 
 public class UserContentReactionFilterParams: FilterBaseParams
 {
-    
+    public bool? LoadUser { get; set; }
+    public bool? LoadPostUser { get; set; }
+    public bool? LoadPostEnterprise { get; set; }
+    public bool? LoadCommentUser { get; set; }
+    public bool? LoadCommentEnterprise { get; set; }
 }
