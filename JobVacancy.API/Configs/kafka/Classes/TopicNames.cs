7dddc1b6ef4897d72b025e46fc2035279a6657cc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 20:37:08 -0300

    New TopicNames

---
+++ JobVacancy.API/Configs/kafka/Classes/TopicNames.cs
@@ -0,0 +1,8 @@
+namespace JobVacancy.API.Configs.kafka.Classes;
+
+public class TopicNames
+{
+    public string? CalculationMetricTopic { get; set; }
+        
+    public string? OtherTopic { get; set; }
+}
