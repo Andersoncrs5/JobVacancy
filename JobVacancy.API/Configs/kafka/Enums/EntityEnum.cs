be3e406ea8610a29d88845eb62ff6c926da5cd2d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-20 19:38:19 -0300

    Added EntityEnum

---
+++ JobVacancy.API/Configs/kafka/Enums/EntityEnum.cs
@@ -0,0 +1,9 @@
+namespace JobVacancy.API.Configs.kafka.Enums;
+
+public enum EntityEnum
+{
+    POST_USER_METRIC = 0,
+    POST_ENTERPRISE_METRIC = 1,
+    COMMENT_POST_ENTERPRISE_METRIC = 2,
+    COMMENT_POST_USER_METRIC = 3,
+}
