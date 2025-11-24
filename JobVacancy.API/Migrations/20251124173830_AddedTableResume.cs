270eb0947390bdb95031c1c30af20d8e083c72e9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-24 14:56:18 -0300

    New migrations

---
+++ JobVacancy.API/Migrations/20251124173830_AddedTableResume.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobVacancy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTableResume : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Resumes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    Name = table.Column<string>(type: "character varying(400)", maxLength: 400, nullable: false),
+                    Url = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    Version = table.Column<int>(type: "integer", nullable: true, defaultValue: 0),
+                    BucketName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ObjectKey = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    userId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resumes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resumes_app_users_userId",
+                        column: x => x.userId,
+                        principalTable: "app_users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resumes_userId",
+                table: "Resumes",
+                column: "userId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resumes");
+        }
+    }
+}
