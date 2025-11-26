7ffdc9c938542601c6e427a88987c743febe01e4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-26 18:39:17 -0300

    .

--- JobVacancy.API/Controllers/UserController.cs
+++ JobVacancy.API/Controllers/UserController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
+using Minio.DataModel.Response;
 
 namespace JobVacancy.API.Controllers;
 
@@ -21,6 +22,7 @@ public class UserController(
     IUserService userService,
     IRolesService rolesService,
     IConfiguration configuration,
+    IMiniOService miniOService,
     IMapper mapper
     ) : Controller
 {
@@ -53,8 +55,7 @@ public class UserController(
                 });
             }
 
-            bool checkName = await userService.ExistsByUsername(dto.Name);
-            if (checkName == true)
+            if (!string.IsNullOrWhiteSpace(dto.Name) && await userService.ExistsByUsername(dto.Name) && user.UserName != dto.Name)
             {
                 return StatusCode(StatusCodes.Status409Conflict, new ResponseHttp<object>
                 {
@@ -67,7 +68,7 @@ public class UserController(
                     Timestamp = DateTimeOffset.UtcNow,
                 });
             }
- 
+
             UserResult userUpdated = await userService.UpdateAsync(user, dto);
 
             UserDto userUpdateDto = mapper.Map<UserDto>(userUpdated.User);
@@ -137,5 +138,71 @@ public class UserController(
             });
         }
     }
-    
+
+    /*
+    [HttpPut("image/profile")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> UpdateImageProfile([FromForm] UpdateUserDto dto)
+    {
+        string bucketName = configuration.GetValue<string>("Buckets:ImageBucketName") ?? throw new ArgumentNullException(nameof(bucketName));
+        string? id = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (id == null) return Unauthorized();
+        
+        UserEntity? user = await userService.GetUserBySid(id);
+        if (user == null) return Unauthorized();
+
+        if (dto.File == null || dto.File.Length == 0) 
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, new ResponseHttp<object>
+            {
+                Data = null,
+                Code = StatusCodes.Status400BadRequest,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Message = "File is required"
+            });
+        }
+
+        PutObjectResponse response = await miniOService.UploadOptimized(bucketName, dto.File);
+        
+        if (response.Size == 0 || dto.File.Length != response.Size)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
+            {
+                Message = "Error the upload image! Try again later.",
+                TraceId = HttpContext.TraceIdentifier,
+                Version = 1,
+                Status = false,
+                Timestamp = DateTimeOffset.UtcNow,
+                Data = null,
+                Code = StatusCodes.Status500InternalServerError
+            });
+        }
+        
+        if (!string.IsNullOrWhiteSpace(user.ImageProfileUrl))
+        {
+            bool existsFile = await miniOService.ExistsFile(bucketName, user.ImageProfileUrl);
+            if (existsFile)
+                await miniOService.RemoveFile(bucketName, user.ImageProfileUrl);
+        }
+
+        user.ImageProfileUrl = response.ObjectName;
+        UserResult result = await userService.UpdateAsync(user);
+
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>
+        {
+            Data = mapper.Map<UserDto>(result.User),
+            Code = StatusCodes.Status200OK,
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            TraceId = HttpContext.TraceIdentifier,
+            Version = 1,
+            Message = "Image has been updated"
+        });
+    }
+    */
 }
