b2a8f86106685297f053242d4347af807f9ed051
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-26 13:57:49 -0300

    added required in columns name, BucketName and ObjectKey

--- JobVacancy.API/models/entities/ResumeEntity.cs
+++ JobVacancy.API/models/entities/ResumeEntity.cs
@@ -2,11 +2,11 @@ namespace JobVacancy.API.models.entities;
 
 public class ResumeEntity: BaseEntity
 {
-    public string Name { get; set; }
-    public string Url { get; set; }
+    public required string Name { get; set; }
+    public string? Url { get; set; }
     public int? Version { get; set; }
-    public string BucketName { get; set; } 
-    public string ObjectKey { get; set; }
+    public required string BucketName { get; set; } 
+    public required string ObjectKey { get; set; }
     
     public string userId { get; set; }
     public UserEntity? User { get; set; }
