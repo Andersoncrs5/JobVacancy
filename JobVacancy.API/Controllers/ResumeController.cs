bb28bbc2e9b8d776d8f3b805947474d038084389
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-26 14:12:56 -0300

    New endpoints

--- JobVacancy.API/Controllers/ResumeController.cs
+++ JobVacancy.API/Controllers/ResumeController.cs
@@ -1,7 +1,12 @@
 
+using System.Security.Claims;
 using AutoMapper;
+using JobVacancy.API.models.dtos.PostUser;
 using JobVacancy.API.models.dtos.Resume;
+using JobVacancy.API.models.entities;
 using JobVacancy.API.Services.Interfaces;
+using JobVacancy.API.Utils.Filters.Resume;
+using JobVacancy.API.Utils.Page;
 using JobVacancy.API.Utils.Res;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -22,56 +27,167 @@ public class ResumeController(
     IMiniOService miniOService,
     IResumeService resumeService,
     IMapper mapper,
-    IMinioClient  minioClient,
     ILogger<ResumeController> logger
 ) : Controller
 {
     
     [HttpPost]
+    [Authorize(Roles = "USER_ROLE")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ResponseHttp<object>))]
     public async Task<IActionResult> Post([FromForm] CreateResumeDto dto, [FromQuery] string? version = null)
     {
-        PutObjectResponse response = await miniOService.UploadOptimized("resumes", dto.File, version);
-        return Ok(response);
-    }
-    
-    [HttpGet("{bucket}/{fileName}")]
-    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<FileStreamResult>))]
-    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
-    public async Task<IActionResult> DownloadFile(string bucket, string fileName)
-    {
-        bool existsBucket = await miniOService.ExistsBucket(bucket);
-        if (!existsBucket) 
-        {
-            return NotFound(new ResponseHttp<object>
-            {
-                Code = StatusCodes.Status404NotFound,
-                Data = null,
-                Message = "Bucket not found",
-                Status = false,
-                Timestamp = DateTimeOffset.UtcNow,
-                TraceId = HttpContext.TraceIdentifier,
-                Version = 1
-            });
-        }
+        string bucketName = configuration.GetValue<string>("Buckets:ResumeBucketName") ?? throw new ArgumentNullException(nameof(bucketName));
         
-        bool existsFile = await miniOService.ExistsFile(bucket, fileName);
-        if (!existsBucket) 
+        string? id = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (id == null) return Unauthorized();
+
+        UserEntity? user = await userService.GetUserBySid(id);
+        if (user == null) return Unauthorized();
+        
+        PutObjectResponse response = await miniOService.UploadOptimized(bucketName, dto.File, version);
+        
+        if (response.Size == 0 || response.Etag == null) 
         {
-            return NotFound(new ResponseHttp<object>
+            return StatusCode(StatusCodes.Status500InternalServerError, new ResponseHttp<object>
             {
-                Code = StatusCodes.Status404NotFound,
-                Data = null,
-                Message = "File not found",
+                Message = "Error the storage file! Try again later.",
                 Status = false,
-                Timestamp = DateTimeOffset.UtcNow,
                 TraceId = HttpContext.TraceIdentifier,
-                Version = 1
+                Code = StatusCodes.Status500InternalServerError,
+                Timestamp = DateTimeOffset.UtcNow,
+                Version = 1,
+                Data = null
             });
         }
 
-        return await miniOService.DownloadFile(bucket, fileName);
+        ResumeEntity entity = await resumeService.Create(dto, user.Id, response);
+
+        return StatusCode(StatusCodes.Status201Created, new ResponseHttp<ResumeDto>
+        {
+            Code = StatusCodes.Status201Created,
+            Data = mapper.Map<ResumeDto>(entity),
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            Version = 1,
+            TraceId = HttpContext.TraceIdentifier,
+            Message = "Resume added"
+        });
     }
- 
-   
+
+    [HttpGet("{id:required}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<ResumeDto>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> Get(string id)
+    {
+        ResumeEntity? entity = await resumeService.GetById(id);
+        if (entity == null) 
+        {
+            return NotFound(new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Message = "Resume not found",
+                Status = false,
+                Data = null,
+                TraceId = HttpContext.TraceIdentifier,
+                Timestamp = DateTimeOffset.UtcNow,
+                Version = 1,
+            });
+        }
+        
+        return Ok(new ResponseHttp<object>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = mapper.Map<ResumeDto>(entity),
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            Version = 1,
+            TraceId = HttpContext.TraceIdentifier,
+            Message = "Resume found",
+        });
+    }
+    
+    [HttpDelete("{id:required}")]
+    [Authorize(Roles = "USER_ROLE")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<object>))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ResponseHttp<object>))]
+    public async Task<IActionResult> Delete(string id)
+    {
+        string bucketName = configuration.GetValue<string>("Buckets:ResumeBucketName") ?? throw new ArgumentNullException(nameof(bucketName));
+        
+        string? userId = User.FindFirst(ClaimTypes.Sid)?.Value;
+        if (userId == null) return Unauthorized();
+
+        UserEntity? user = await userService.GetUserBySid(userId);
+        if (user == null) return Unauthorized();
+        
+        ResumeEntity? entity = await resumeService.GetById(id);
+        if (entity == null) 
+        {
+            return NotFound(new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Message = "Resume not found",
+                Status = false,
+                Data = null,
+                TraceId = HttpContext.TraceIdentifier,
+                Timestamp = DateTimeOffset.UtcNow,
+                Version = 1,
+            });
+        }
+
+        if (entity.userId != userId) return Forbid();
+        
+        bool existsFile = await miniOService.ExistsFile(bucketName, entity.ObjectKey);
+        if (!existsFile) 
+        {
+            await resumeService.Delete(entity);
+            
+            return NotFound(new ResponseHttp<object>
+            {
+                Code = StatusCodes.Status404NotFound,
+                Message = "Resume not found",
+                Status = false,
+                Data = null,
+                TraceId = HttpContext.TraceIdentifier,
+                Timestamp = DateTimeOffset.UtcNow,
+                Version = 1,
+            });
+        }
+
+        await miniOService.RemoveFile(bucketName, entity.ObjectKey);
+        
+        await resumeService.Delete(entity);
+        
+        return Ok(new ResponseHttp<object>
+        {
+            Code = StatusCodes.Status200OK,
+            Data = null,
+            Status = true,
+            Timestamp = DateTimeOffset.UtcNow,
+            Version = 1,
+            TraceId = HttpContext.TraceIdentifier,
+            Message = "Resume removed",
+        });
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ResponseHttp<Page<ResumeDto>>))]
+    public async Task<IActionResult> GetAll([FromQuery] ResumeFilterParams filter)
+    {
+        IQueryable<ResumeEntity> iQueryable = resumeService.Query();
+        IQueryable<ResumeEntity> appliedFilter = ResumeFilterQuery.ApplyFilter(iQueryable, filter);
+        PaginatedList<ResumeEntity> paginatedList = await PaginatedList<ResumeEntity>.CreateAsync(
+            source: appliedFilter,
+            pageSize: filter.PageSize,
+            pageIndex: filter.PageNumber
+        );
+        
+        Page<ResumeDto> dtos = mapper.Map<Page<ResumeDto>>(paginatedList);
+        
+        return Ok(dtos);
+    }
     
 }
