9e013147ffa396a7e3d1e271ea6b2fb15e1da4ad
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-25 21:45:34 -0300

    Add column File

--- JobVacancy.API/models/dtos/Resume/CreateResumeDto.cs
+++ JobVacancy.API/models/dtos/Resume/CreateResumeDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace JobVacancy.API.models.dtos.Resume;
 
 public class CreateResumeDto
 {
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public int? Version { get; set; }
+    [Required]
+    public required IFormFile File { get; set; }
 }
