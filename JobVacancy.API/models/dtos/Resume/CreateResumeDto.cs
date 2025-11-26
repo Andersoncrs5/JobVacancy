56b178c5cf5444651e8c8eed16aebaf9f10cf38b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-26 13:56:11 -0300

    Added validations

--- JobVacancy.API/models/dtos/Resume/CreateResumeDto.cs
+++ JobVacancy.API/models/dtos/Resume/CreateResumeDto.cs
@@ -1,11 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using JobVacancy.API.Utils.Annotations.Validations.Global;
 
 namespace JobVacancy.API.models.dtos.Resume;
 
 public class CreateResumeDto
 {
+    [Required]
     public required string Name { get; set; }
+    [Range(0, 100)]
     public int? Version { get; set; }
     [Required]
+    [MaxFileSize(10)]
+    [AllowedExtensions([".pdf", ".docx", ".doc", ".odt", ".rtf", ".txt", ".png", ".jpg", ".jpeg", ".htm", ".html"])]
     public required IFormFile File { get; set; }
 }
