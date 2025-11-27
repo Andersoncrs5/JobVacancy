e9e193210c3f07eef24f2b2c70254097bc218d2c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-27 19:56:32 -0300

    removed validations

--- JobVacancy.API/models/dtos/Base/MediaBase.cs
+++ JobVacancy.API/models/dtos/Base/MediaBase.cs
@@ -2,21 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace JobVacancy.API.models.dtos.Base;
 
-public class MediaBase
+public class MediaBase: BaseDto
 {
-    [Required]
-    [MaxLength(450)]
     public required string ObjectName { get; set; }
-    
-    [Required]
-    [MaxLength(100)]
     public required string BucketName { get; set; }
-    
-    [MaxLength(3)]
     public string? VersionImage { get; set; }
-    
     public long? FileSizeBytes { get; set; }
-    
-    [Range(0,15)]
     public int? Order { get; set; }
 }
