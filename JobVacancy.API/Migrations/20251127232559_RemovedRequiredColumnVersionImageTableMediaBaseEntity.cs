cfaf73dd64aa88e2b3eee8e9a7bb74439358860b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-11-27 21:02:12 -0300

    New migrations

---
+++ JobVacancy.API/Migrations/20251127232559_RemovedRequiredColumnVersionImageTableMediaBaseEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobVacancy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedRequiredColumnVersionImageTableMediaBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VersionImage",
+                table: "Medias",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(3)",
+                oldMaxLength: 3);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VersionImage",
+                table: "Medias",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(3)",
+                oldMaxLength: 3,
+                oldNullable: true);
+        }
+    }
+}
